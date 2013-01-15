d76fcbbed1733ce1e933e00a2a15cae4f0d0f6ec
John Bachman <bachmanjohn@gmail.com>
2013-01-14 22:25:17 -0500

    Scripts to submit jobs on orchestra

---
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -0,0 +1,4 @@
+for i in range(0, 5):
+    cmdstr = "bsub -q sorger_2d -o nbc_mcmc_%d.out python nbd_linear_mcmc.py " \
+             "nbd_parallel_random_initial_values.pck %d nbd_mcmc" % (i, i)
+    print cmdstr
