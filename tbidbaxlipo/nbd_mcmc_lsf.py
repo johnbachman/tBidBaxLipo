e7b961666dac17099c9d2dce6b9097ea1658e898
John Bachman <bachmanjohn@gmail.com>
2013-01-14 23:28:07 -0500

    Added email directive to bsub commands

--- tbidbaxlipo/nbd_mcmc_lsf.py
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -1,4 +1,5 @@
 for i in range(0, 5):
-    cmdstr = "bsub -q sorger_1d -o nbc_mcmc_%d.out python nbd_linear_mcmc.py " \
+    cmdstr = "bsub -q sorger_1d -o nbc_mcmc_%d.out -u bachman@fas.harvard.edu "\
+             "python nbd_linear_mcmc.py " \
              "nbd_parallel_random_initial_values.pck %d nbd_mcmc" % (i, i)
     print cmdstr
