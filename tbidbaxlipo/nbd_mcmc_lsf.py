e34aadae2040e66262716a5285d7b8e5e8a285a0
John Bachman <bachmanjohn@gmail.com>
2013-01-21 11:43:25 -0500

    Modified lsf scripts to use nbd_mcmc filename

--- tbidbaxlipo/nbd_mcmc_lsf.py
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -1,5 +1,4 @@
 for i in range(0, 5):
-    cmdstr = "bsub -q sorger_1d -o nbc_mcmc_%d.out -u bachman@fas.harvard.edu "\
-             "python nbd_linear_mcmc.py " \
-             "nbd_parallel_random_initial_values.pck %d nbd_mcmc" % (i, i)
+    cmdstr = "bsub -q sorger_1d python nbd_mcmc.py " \
+             "nbd_parallel_random_initial_values.pck %d nbd_mcmc" % i
     print cmdstr
--- tbidbaxlipo/nbd_mcmc_lsf.sh
+++ tbidbaxlipo/nbd_mcmc_lsf.sh
@@ -1,5 +1,5 @@
-bsub -q sorger_1d -o nbc_mcmc_0.out -u bachman@fas.harvard.edu python nbd_linear_mcmc.py nbd_parallel_random_initial_values.pck 0 nbd_mcmc
-bsub -q sorger_1d -o nbc_mcmc_1.out -u bachman@fas.harvard.edu python nbd_linear_mcmc.py nbd_parallel_random_initial_values.pck 1 nbd_mcmc
-bsub -q sorger_1d -o nbc_mcmc_2.out -u bachman@fas.harvard.edu python nbd_linear_mcmc.py nbd_parallel_random_initial_values.pck 2 nbd_mcmc
-bsub -q sorger_1d -o nbc_mcmc_3.out -u bachman@fas.harvard.edu python nbd_linear_mcmc.py nbd_parallel_random_initial_values.pck 3 nbd_mcmc
-bsub -q sorger_1d -o nbc_mcmc_4.out -u bachman@fas.harvard.edu python nbd_linear_mcmc.py nbd_parallel_random_initial_values.pck 4 nbd_mcmc
+bsub -q sorger_1d python nbd_mcmc.py nbd_parallel_random_initial_values.pck 0 nbd_mcmc
+bsub -q sorger_1d python nbd_mcmc.py nbd_parallel_random_initial_values.pck 1 nbd_mcmc
+bsub -q sorger_1d python nbd_mcmc.py nbd_parallel_random_initial_values.pck 2 nbd_mcmc
+bsub -q sorger_1d python nbd_mcmc.py nbd_parallel_random_initial_values.pck 3 nbd_mcmc
+bsub -q sorger_1d python nbd_mcmc.py nbd_parallel_random_initial_values.pck 4 nbd_mcmc
