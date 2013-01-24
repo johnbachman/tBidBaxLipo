44fd87afc96897c8777afe2a4b2015cb2d67f59f
John Bachman <bachmanjohn@gmail.com>
2013-01-24 13:59:56 -0500

    Made lsf file for c3 only

--- tbidbaxlipo/nbd_mcmc_lsf.py
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -1,4 +1,4 @@
 for i in range(0, 5):
     cmdstr = "bsub -q sorger_1d python nbd_mcmc.py " \
-             "nbd_parallel_random_initial_values.pck %d nbd_mcmc" % i
+             "nbd_mcmc_c3_random_initial_values.pck %d nbd_mcmc" % i
     print cmdstr
