d5c08acefcba22d0d76a90be5a3e58bf8c5b7e43
John Bachman <bachmanjohn@gmail.com>
2013-01-28 16:08:36 -0500

    Pre-run the models before making the lsf script

--- tbidbaxlipo/nbd_mcmc_lsf.py
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -1,21 +1,38 @@
 import sys
+import numpy as np
+from pysb.integrate import odesolve
+
+# The calls to odesolve are done to insure that the chains don't crash on each other
+# trying to run scipy.weave at the same time.
 
 num_chains = 20
 queue = 'sorger_1d'
+t = np.linspace(0, 60*60*3, 100)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Please specify a fit type (c3, c3c120, or linear)."
     else:
         if sys.argv[1] == 'c3':
+            from nbd_parallel_model import model
+            x = odesolve(model, t)            
             for i in range(0, num_chains):
                 cmdstr = "bsub -q %s python nbd_mcmc.py " \
                          "nbd_mcmc_c3_random_initial_values.pck %d nbd_mcmc" % \
                          (queue, i)
                 print cmdstr
         elif sys.argv[1] == 'c3c120':
-            print "Not implemented yet"
+            from nbd_parallel_model import model
+            x = odesolve(model, t)            
+            for i in range(0, num_chains):
+                cmdstr = "bsub -q %s python nbd_mcmc_c3c120.py " \
+                         "nbd_mcmc_c3c120_random_initial_values.pck %d " \
+                         "nbd_mcmc_c3c120" % \
+                         (queue, i)
+                print cmdstr
         elif sys.argv[1] == 'linear':
+            from nbd_linear_model import model
+            x = odesolve(model, t)            
             for i in range(0, num_chains):
                 cmdstr = "bsub -q %s python nbd_mcmc.py " \
                          "nbd_linear_random_initial_values.pck %d " \
