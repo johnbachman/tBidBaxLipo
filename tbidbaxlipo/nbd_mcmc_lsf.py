eb9e43182bda76ffedc38607a35880a905535b05
John Bachman <bachmanjohn@gmail.com>
2013-01-29 10:51:43 -0500

    Modified nbd_mcmc_lsf to run bsub from within python

--- tbidbaxlipo/nbd_mcmc_lsf.py
+++ tbidbaxlipo/nbd_mcmc_lsf.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from pysb.integrate import odesolve
+import subprocess
 
 # The calls to odesolve are done to insure that the chains don't crash on each other
 # trying to run scipy.weave at the same time.
@@ -13,31 +14,50 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Please specify a fit type (c3, c3c120, or linear)."
     else:
+        # Fit parallel model with c3 only
         if sys.argv[1] == 'c3':
             from nbd_parallel_model import model
             x = odesolve(model, t)            
             for i in range(0, num_chains):
-                cmdstr = "bsub -q %s python nbd_mcmc.py " \
-                         "nbd_mcmc_c3_random_initial_values.pck %d nbd_mcmc" % \
-                         (queue, i)
-                print cmdstr
+                cmd_list = ["bsub", "-q", queue, "python", "nbd_mcmc_c3.py",
+                            "nbd_mcmc_c3_random_initial_values.pck",
+                            str(i), "nbd_mcmc_c3"]
+                print ' '.join(cmd_list)
+                subprocess.call(cmd_list)
+
+        # Fit parallel model with c3 and c120 only
         elif sys.argv[1] == 'c3c120':
             from nbd_parallel_model import model
             x = odesolve(model, t)            
             for i in range(0, num_chains):
-                cmdstr = "bsub -q %s python nbd_mcmc_c3c120.py " \
-                         "nbd_mcmc_c3c120_random_initial_values.pck %d " \
-                         "nbd_mcmc_c3c120" % \
-                         (queue, i)
-                print cmdstr
+                cmd_list = ["bsub", "-q", queue, "python", "nbd_mcmc_c3c120.py",
+                            "nbd_mcmc_c3c120_random_initial_values.pck",
+                            str(i), "nbd_mcmc_c3c120"]
+                print ' '.join(cmd_list)
+                subprocess.call(cmd_list)
+
+        # Fit parallel model
+        elif sys.argv[1] == 'parallel':
+            from nbd_parallel_model import model
+            x = odesolve(model, t)            
+            for i in range(0, num_chains):
+                cmd_list = ["bsub", "-q", queue, "python", "nbd_mcmc.py",
+                            "nbd_mcmc_parallel_random_initial_values.pck",
+                            str(i), "nbd_mcmc_parallel"]
+                print ' '.join(cmd_list)
+                subprocess.call(cmd_list)
+
+        # Fit linear model
         elif sys.argv[1] == 'linear':
             from nbd_linear_model import model
             x = odesolve(model, t)            
             for i in range(0, num_chains):
-                cmdstr = "bsub -q %s python nbd_mcmc.py " \
-                         "nbd_linear_random_initial_values.pck %d " \
-                         "nbd_mcmc_linear" % \
-                         (queue, i)
-                print cmdstr
+                cmd_list = ["bsub", "-q", queue, "python", "nbd_mcmc.py",
+                            "nbd_mcmc_linear_random_initial_values.pck",
+                            str(i), "nbd_mcmc_linear"]
+                print ' '.join(cmd_list)
+                subprocess.call(cmd_list)
+
+        # Error
         else:
             print "Not a known fit type."
