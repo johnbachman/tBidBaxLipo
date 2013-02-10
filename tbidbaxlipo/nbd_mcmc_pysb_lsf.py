2c28c0a430560a78bfa6cf79ca82771d3c1cb40f
John Bachman <bachmanjohn@gmail.com>
2013-02-10 14:27:28 -0500

    LSF submit script for the pysb NBD models

---
+++ tbidbaxlipo/nbd_mcmc_pysb_lsf.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+from pysb.integrate import odesolve
+import subprocess
+import time
+
+num_chains = 20
+queue = 'sorger_1d'
+
+if __name__ == '__main__':
+    for i in range(0, num_chains):
+        cmd_list = ["bsub", "-q", queue,
+                    "python", "nbd_mcmc_pysb.py",
+                    "random_seed=%d" % i]
+        cmd_list += sys.argv[1:]
+        print ' '.join(cmd_list)
+        subprocess.call(cmd_list)
