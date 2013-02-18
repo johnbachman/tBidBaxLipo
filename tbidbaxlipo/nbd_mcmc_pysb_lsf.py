0c80b18bb63725050feeebb5f212e6d1fa2dfe74
John Bachman <bachmanjohn@gmail.com>
2013-02-18 17:09:40 -0500

    Added output file commnd-line argument

--- tbidbaxlipo/nbd_mcmc_pysb_lsf.py
+++ tbidbaxlipo/nbd_mcmc_pysb_lsf.py
@@ -1,12 +1,20 @@
 import sys
 import subprocess
 
-num_chains = 20
+num_chains = 10
 queue = 'short'
 
 if __name__ == '__main__':
+    # Keyword args are set at the command line as e.g., key=val
+    # and subsequently split at the equals sign
+    kwargs = dict([arg.split('=') for arg in sys.argv[1:]])
+
     for i in range(0, num_chains):
+        output_filename = '%s_%s_%s_%d_s%d' % \
+                (kwargs['model'], kwargs['nbd_site'], kwargs['nbd_observable'],
+                 kwargs['nsteps'], i)
         cmd_list = ["bsub", "-q", queue, "-W", "12:00",
+                    "-o", output_filename,
                     "python", "nbd_mcmc_pysb.py",
                     "random_seed=%d" % i]
         cmd_list += sys.argv[1:]
