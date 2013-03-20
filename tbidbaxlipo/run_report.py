903b5fd984f94720e9fee0506a1fcdbf2d95feb8
John Bachman <bachmanjohn@gmail.com>
2013-03-19 21:14:31 -0400

    Run report includes all reports and dumps to pickle

--- tbidbaxlipo/run_report.py
+++ tbidbaxlipo/run_report.py
@@ -2,20 +2,22 @@ import glob
 from bayessb.report import Report
 import bayessb.report.reporters
 import tbidbaxlipo.reporters
-from tbidbaxlipo.reporters import knowledge, topology
+from tbidbaxlipo.reporters import knowledge, topology, experiment
 from tbidbaxlipo.nbd_mcmc_pysb import import_mcmc_groups
 import sys
+import pickle
 
-chain_file_list = glob.glob('/Volumes/data/computation/Bachman/c3_testrun/' +
-                            'tard_c3_*.mcmc')
-chain_file_list = glob.glob('/Volumes/data/computation/Bachman/c3_testrun/' +
-                            'tard_c3_iBax_50000_s9.mcmc')
-#chain_file_list = glob.glob('/files/ImStor/sorger/data/computation/Bachman/' +
-#                            'tard_c62_*.mcmc')
+#chain_file_list = glob.glob('/Volumes/data/computation/Bachman/'
+#                            'c62_Baxbh3_nlkl/taird*.mcmc')
+chain_file_list = glob.glob('/Volumes/data/computation/Bachman/'
+                            'c62_Baxbh3_nlkl/taird_c62_iBax_50000_s0.mcmc')
+#chain_file_list = glob.glob('ta*.mcmc')
+
 chain_dict = import_mcmc_groups(chain_file_list)
 
-#rep = Report(chain_dict, [bayessb.report.reporters])
-rep = Report(chain_dict, [bayessb.report.reporters,
-                         topology, knowledge])
+rep = Report(chain_dict, [topology, bayessb.report.reporters,
+                          knowledge, experiment])
 rep.write_html_table_with_links('htmlreport.html')
 
+with open('report.rep', 'w') as f:
+    pickle.dump(rep, f)
