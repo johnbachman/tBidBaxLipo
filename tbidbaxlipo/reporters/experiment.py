f75bb7aa1eaf189026c7c6efd99832347d7a3df7
John Bachman <bachmanjohn@gmail.com>
2013-03-27 12:37:52 -0400

    Tests for changes in tBid concentration

--- tbidbaxlipo/reporters/experiment.py
+++ tbidbaxlipo/reporters/experiment.py
@@ -6,14 +6,27 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 reporter_group_name = 'Experiments'
 num_samples = 100
 
+@reporter('tBid 0.1x')
+def increase_tBid_01x(mcmc_set):
+    """ .. todo:: document the basis for this"""
+    return change_tBid_concentration(mcmc_set, 0.1)
+
+@reporter('tBid 0.5x')
+def increase_tBid_05x(mcmc_set):
+    """ .. todo:: document the basis for this"""
+    return change_tBid_concentration(mcmc_set, 0.5)
+
 @reporter('tBid 10x')
 def increase_tBid_10x(mcmc_set):
     """ .. todo:: document the basis for this"""
+    return change_tBid_concentration(mcmc_set, 10)
+
+def change_tBid_concentration(mcmc_set, fold_change):
     tspan = mcmc_set.chains[0].options.tspan
     fig = Figure()
     ax = fig.gca()
-    plot_filename = '%s_tBid_10x.png' % mcmc_set.name
-    thumbnail_filename = '%s_tBid_10x_th.png' % mcmc_set.name
+    plot_filename = '%s_tBid_%.1fx.png' % (mcmc_set.name, fold_change)
+    thumbnail_filename = '%s_tBid_%.1fx_th.png' % (mcmc_set.name, fold_change)
 
     # Make sure we can call the method 'get_observable_timecourse'
     if not hasattr(mcmc_set.chains[0], 'get_observable_timecourse') or \
@@ -29,10 +42,10 @@ def increase_tBid_10x(mcmc_set):
         x = mcmc_set.chains[0].get_observable_timecourse(position=position)
         ax.plot(tspan, x, color='g', alpha=0.5)
 
-    # Now increase tBid 10-fold...
+    # Now change tBid x-fold...
     model = mcmc_set.chains[0].options.model
     old_tBid_0 = model.parameters['tBid_0'].value
-    model.parameters['tBid_0'].value = old_tBid_0 * 10
+    model.parameters['tBid_0'].value = old_tBid_0 * fold_change
 
     # ...and do the sampling again
     for i in range(num_samples):
