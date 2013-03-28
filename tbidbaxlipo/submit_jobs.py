11552fe13da4d0a8be9fe486df1d48b4726aad26
John Bachman <bachmanjohn@gmail.com>
2013-03-28 15:50:00 -0400

    New submit script for fitting c3 and c62

--- tbidbaxlipo/submit_jobs.py
+++ tbidbaxlipo/submit_jobs.py
@@ -1,22 +1,26 @@
-from nbd_mcmc_pysb import model_names
+#from nbd_mcmc_pysb import model_names
 from tbidbaxlipo.models.one_cpt import Builder
 import subprocess
+
+model_names = ['tairdt']
 
 b = Builder()
 
 nsteps = 150000
 
-nbd_site_names = ['c62']
-nbd_observables = ['iBax', 'Baxbh3', 'Bax2', 'tBidBax']
+nbd_site_combos = [('c3', 'c62')]
+nbd_obs_combos = [('iBax', 'Baxbh3'), ('iBax', 'Bax2')]
 
 cmd_list = []
 for model in model_names:
-    for nbd_site_name in nbd_site_names:
-        for nbd_observable in nbd_observables:
+    for nbd_site_combo in nbd_site_combos:
+        nbd_site_str = '-'.join(nbd_site_combo)
+        for nbd_obs_combo in nbd_obs_combos:
+            nbd_obs_str = '-'.join(nbd_obs_combo)
             cmd_list = ["python", "-m", "tbidbaxlipo.nbd_mcmc_pysb_lsf",
                         "model=%s" % model,
-                        "nbd_sites=%s" % nbd_site_name,
-                        "nbd_observables=%s" % nbd_observable,
+                        "nbd_sites=%s" % nbd_site_str,
+                        "nbd_observables=%s" % nbd_obs_str,
                         "nsteps=%d" % nsteps]
             print ' '.join(cmd_list)
             subprocess.call(cmd_list)
