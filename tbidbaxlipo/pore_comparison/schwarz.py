eeb4de8747f55a42bb91f736ebbc4778aa1d26fa
John Bachman <bachmanjohn@gmail.com>
2013-05-10 11:51:35 -0400

    Liposome titration plot for simple Bax translocation model

--- tbidbaxlipo/pore_comparison/schwarz.py
+++ tbidbaxlipo/pore_comparison/schwarz.py
@@ -3,13 +3,15 @@ from matplotlib import pyplot as plt
 from tbidbaxlipo.models.one_cpt import Builder
 from pysb.integrate import odesolve
 
-bax_concs = np.logspace(-1, 3, 10)
-b = Builder()
-b.build_model_bax_schwarz()
-
 def plot_Bax_titration():
+
+    b = Builder()
+    b.build_model_bax_schwarz()
+
+    bax_concs = np.logspace(-1, 3, 10)
     t = np.linspace(0, 12000, 1000)
     initial_rates = []
+
     for bax_conc in bax_concs:
         b.model.parameters['Bax_0'].value = bax_conc
         x = odesolve(b.model, t)
@@ -25,7 +27,7 @@ def plot_Bax_titration():
     fitted_initial_rates = (slope * log_concs) + intercept
 
     # Figure with slope and intercept info
-    plt.figure(2)
+    plt.figure()
     plt.figtext(0.2, 0.8, 'Slope: %.4f' % slope)
     plt.figtext(0.2, 0.75, 'Intercept: %.4f' % intercept)
     plt.plot(log_concs, log_initial_rates, 'bx')
@@ -35,5 +37,32 @@ def plot_Bax_titration():
     plt.title("Log-Log plot of initial rate vs. Bax conc")
     plt.show()
 
+def plot_liposome_titration():
+    b = Builder()
+    b.translocate_Bax()
+    t = np.linspace(0, 3000, 1000)
+    lipo_concs = np.logspace(-2, 2, 20)
+    ss_mBax_values = []
+
+    # Plot timecourses
+    plt.ion()
+    plt.figure()
+
+    for lipo_conc in lipo_concs:
+        b.model.parameters['Vesicles_0'].value = lipo_conc
+        x = odesolve(b.model, t)
+        mBax_frac = x['mBax'] / b.model.parameters['Bax_0'].value
+        plt.plot(t, mBax_frac)
+        ss_mBax_value = mBax_frac[-1]
+        ss_mBax_values.append(ss_mBax_value)
+    plt.show()
+    ss_mBax_values = np.array(ss_mBax_values)
+
+    # Plot liposome titration
+    plt.figure()
+    plt.plot(lipo_concs, ss_mBax_values)
+    plt.show()
+
 if __name__ == '__main__':
-    plot_Bax_titration()
+    #plot_Bax_titration()
+    plot_liposome_titration()
