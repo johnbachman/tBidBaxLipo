fb537609e8ca4bcfd09b90f6f5b8cf086f439bf3
John Bachman <bachmanjohn@gmail.com>
2013-07-21 13:50:15 -0400

    Added more jobs to mpi_submit.sh

--- tbidbaxlipo/mcmc/mpi_submit.sh
+++ tbidbaxlipo/mcmc/mpi_submit.sh
@@ -1,3 +1,6 @@
 for i in `seq 10`; do
-    bsub -a openmpi -W 24:00 -n 13 -q parallel mpirun.lsf /home/jab69/virtualenvs/pysb/bin/python /home/jab69/projects/tBidBaxLipo/tbidbaxlipo/mcmc/nbd_mcmc_mpi_run.py nsteps=1000000 nbd_observables=iBax nbd_sites=c62 random_seed=$i model=taid
+    bsub -a openmpi -W 24:00 -n 9 -q parallel mpirun.lsf /home/jab69/virtualenvs/pysb/bin/python /home/jab69/projects/tBidBaxLipo/tbidbaxlipo/mcmc/nbd_plate_mcmc_mpi_run.py nsteps=500000 nbd_site=c175 num_confs=3 replicate=2 random_seed=$i
+    bsub -a openmpi -W 24:00 -n 9 -q parallel mpirun.lsf /home/jab69/virtualenvs/pysb/bin/python /home/jab69/projects/tBidBaxLipo/tbidbaxlipo/mcmc/nbd_plate_mcmc_mpi_run.py nsteps=500000 nbd_site=c179 num_confs=3 replicate=2 random_seed=$i
+    bsub -a openmpi -W 24:00 -n 9 -q parallel mpirun.lsf /home/jab69/virtualenvs/pysb/bin/python /home/jab69/projects/tBidBaxLipo/tbidbaxlipo/mcmc/nbd_plate_mcmc_mpi_run.py nsteps=500000 nbd_site=c5 num_confs=3 replicate=2 random_seed=$i
+    bsub -a openmpi -W 24:00 -n 9 -q parallel mpirun.lsf /home/jab69/virtualenvs/pysb/bin/python /home/jab69/projects/tBidBaxLipo/tbidbaxlipo/mcmc/nbd_plate_mcmc_mpi_run.py nsteps=500000 nbd_site=c15 num_confs=3 replicate=2 random_seed=$i
 done
