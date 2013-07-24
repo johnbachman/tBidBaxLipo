0f0efb09cdacba1b2e859cc133b8384fb342d4ae
John Bachman <bachmanjohn@gmail.com>
2013-07-24 14:58:04 -0400

    Docstring comments for models modules

---
+++ tbidbaxlipo/models/__init__.py
@@ -0,0 +1,4 @@
+"""
+Models of translocation, activation, oligomerization, and permeabilization
+kinetics of tBid, Bax and liposomes measured in vitro.
+"""
---
+++ tbidbaxlipo/models/nbd/__init__.py
@@ -0,0 +1,5 @@
+"""
+Models explicitly aimed at modeling data from experiments measuring
+fluorescence kinetics for Bax mutants labeled with the environment-sensitive
+dye NBD.
+"""
