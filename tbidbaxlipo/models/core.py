99c3643fe7e742bcf9eaf92576e1b966900fb12e
John Bachman <bachmanjohn@gmail.com>
2013-07-28 14:44:30 -0400

    Removed builder functions from core and moved to pysb.builder

--- tbidbaxlipo/models/core.py
+++ tbidbaxlipo/models/core.py
@@ -9,7 +9,7 @@ Code organization
 Models of tBid/Bax interactions draw on the same pool of mechanistic facts, but
 may differ in their implementation of how interactions with membranes are
 modeled. As a result, some rules (e.g., those that specify how proteins
-translocate to membranes) have to be written in a compartmen-specific way,
+translocate to membranes) have to be written in a compartment-specific way,
 whereas others can be written in a generic way. To manage this degree of
 complexity, macros implementing tBid/Bax interactions are implemented here in a
 generic way (e.g.,
@@ -19,35 +19,13 @@ in child classes (e.g.,
 :py:meth:`tbidbaxlipo.models.site_cpt.Builder.tBid_activates_Bax`).
 
 The model builder classes contained in this file and also in
-:py:mod:`tbidbaxlipo.models.one_cpt`, :py:mod:`tbidbaxlipo.models.n_cpt`,
-and :py:mod:`tbidbaxlipo.models.site_cpt`, are used to manage the process of
+:py:mod:`tbidbaxlipo.models.one_cpt`, :py:mod:`tbidbaxlipo.models.n_cpt`, and
+:py:mod:`tbidbaxlipo.models.site_cpt`, are used to manage the process of
 building up alternative models. Each of these modules contains a class
 `Builder` that contains an instance of a PySB model. The classes also contain a
 series of methods implementing small sub-pieces of mechanism that can be termed
 "motifs". These motifs can be recombined in different ways to create different
 models.
-
-The pattern for model construction used here does not rely on the SelfExporter
-class of PySB. Instead, the ``Builder`` class contains an instance of a PySB
-model object. Monomers, Parameters, Rules, etc. are added to this model object
-by invoking the wrapper functions included in
-:py:mod:`tbidbaxlipo.model.core`. These include
-
-- :py:meth:`tbidbaxlipo.model.core.Builder.monomer`
-- :py:meth:`tbidbaxlipo.model.core.Builder.parameter`
-- :py:meth:`tbidbaxlipo.model.core.Builder.rule`
-- :py:meth:`tbidbaxlipo.model.core.Builder.compartment`
-- :py:meth:`tbidbaxlipo.model.core.Builder.initial`
-- :py:meth:`tbidbaxlipo.model.core.Builder.observable`
-
-Each of these functions invokes the appropriate PySB component constructor
-while setting the argument ``_export=False`` so that the SelfExporter is not
-invoked. The created components are then added to the instance of the model
-contained in the builder class.
-
-In addition, the model builder class implements the ``__getitem__`` method so
-that invoking ``self['component_name']`` from within any method returns the
-component with the given name.
 
 Individual mechanisms implemented
 =================================
@@ -267,12 +245,12 @@ paradigm.
 
 __author__ = 'johnbachman'
 
-from pysb import *
-from pysb.macros import *
+#from pysb import *
+#from pysb.macros import *
 import numpy as np
-from bayessb.priors import Normal
+import pysb.builder
 
-class Builder(object):
+class Builder(pysb.builder.Builder):
 
     # -- VIRTUAL FUNCTIONS ----------------------------------------------------
     def within_compartment_rsf(self):
@@ -283,33 +261,6 @@ class Builder(object):
 
     def run_model(self):
         raise NotImplementedError()
-
-    # -- CONSTRUCTOR AND MONOMER DECLARATIONS --------------------------------
-    def __init__(self, params_dict=None):
-        """Base constructor for all model builder classes.
-
-        Initializes collections of the parameters to estimate, as well
-        as the means and variances of their priors.
-
-        Parameters
-        ----------
-        params_dict : dict
-            The params_dict allows any parameter value to be overriden
-            by name; any parameters not included in the dict will be set
-            to default values. For example, if params_dict contains::
-
-                {'tBid_Bax_kf': 1e-2}
-
-            then the parameter tBid_Bax_kf will be assigned a value of 1e-2;
-            all other parameters will take on default values. However,
-            note that the parameter value given will be multiplied by any
-            scaling factor passed in when the parameter is declared.
-        """
-
-        self.model = Model('tBid_Bax', _export=False)
-        self.estimate_params = []
-        self.params_dict = params_dict
-        self.priors = []
 
     def declare_monomers(self):
         """Declares signatures for tBid and Bax."""
@@ -878,7 +829,6 @@ class Builder(object):
         self.pores_from_Bax_dimers(bax_loc_state='i', reversible=True)
         self.model.name = 'bax_heat_auto_dimer_reversible'
 
-
     # Models incorporating dye release
     def build_model_bax_schwarz(self):
         self.translocate_Bax()
@@ -917,96 +867,4 @@ class Builder(object):
         self.model.name = 'tap1'
 
 
-
-    # -- CONSTRUCTOR WRAPPER FUNCTIONS ---------------------------------------
-    def monomer(self, *args, **kwargs):
-        """Adds a parameter to the Builder's model instance."""
-        m = Monomer(*args, _export=False, **kwargs)
-        self.model.add_component(m)
-        return m
-
-    def parameter(self, name, value, factor=1, estimate=True,
-                  prior=Normal(-3, 2)):
-        """Adds a parameter to the Builder's model instance.
-
-        Examines the params_dict attribute of the Builder instance (which is
-        set in the constructor,
-        :py:meth:`tbidbaxlipo.models.core.Builder.__init__`).  If the
-        parameter with the given name is in the ``params_dict``, then the value
-        in the ``params_dict`` is used to construct the parameter, and the
-        argument ``value`` is ignored. If the parameter is not in
-        ``params_dict``, then the parameter is assigned ``value``.
-
-        Furthermore, in all cases the parameter value is multiplied by a
-        scaling factor specified by the argument ``factor``. This allows rate
-        scaling factor that are dependent on lipid concentrations or on units
-        (deterministic vs. stochastic) to be handled by keeping the same
-        parameter value but passing in the appropriate value for ``factor``.
-
-        Parameters
-        ----------
-        name : string
-            The name of the parameter to add
-        value : number
-            The value of the parameter
-        factor : number
-            A scaling factor to be applied to the parameter value.
-        estimate : boolean
-            Specifies whether the parameter should be included among the
-            parameters to estimate, contained in the set
-            ``Builder.estimate_params``. Defaults to True.
-        prior : instance of prior class from tbidbaxlipo.models.priors
-            The prior object determining the prior probability of different
-            values for this parameter. Ignored if ``estimate`` is False.
-        """
-
-        if self.params_dict is None:
-            param_val = value * factor
-        else:
-            if name in self.params_dict:
-                param_val = self.params_dict[name] * factor
-            else:
-                param_val = value * factor
-
-        p = Parameter(name, param_val, _export=False)
-        self.model.add_component(p)
-
-        if estimate:
-            self.estimate_params.append(p)
-            self.priors.append(prior)
-
-        return p
-
-    def rule(self, *args, **kwargs):
-        """Adds a rule to the Builder's model instance."""
-        r = Rule(*args, _export=False, **kwargs)
-        self.model.add_component(r)
-        return r 
-
-    def compartment(self, *args, **kwargs):
-        """Adds a compartment to the Builder's model instance."""
-        c = Compartment(*args, _export=False, **kwargs)
-        self.model.add_component(c)
-        return c 
-
-    def observable(self, *args, **kwargs):
-        """Adds an observable to the Builder's model instance."""
-        o = Observable(*args, _export=False, **kwargs)
-        self.model.add_component(o)
-        return o
-
-    def expression(self, *args, **kwargs):
-        """Adds an expression to the Builder's model instance."""
-        e = Expression(*args, _export=False, **kwargs)
-        self.model.add_component(e)
-        return e
-
-    def initial(self, *args):
-        """Adds an initial condition to the Builder's model instance."""
-        self.model.initial(*args)
-
-    def __getitem__(self, index):
-        """Returns the component with the given string index
-        from the instance of the model contained by the Builder."""
-        return self.model.all_components()[index]
 
