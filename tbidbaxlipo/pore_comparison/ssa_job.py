dfba4c97a5416026f7deb035d249e6d0014148cf
John Bachman <bachmanjohn@gmail.com>
2013-08-01 16:52:59 -0400

    Rolled submit_ssa_jobs into ssa_job as an option

--- tbidbaxlipo/pore_comparison/ssa_job.py
+++ tbidbaxlipo/pore_comparison/ssa_job.py
@@ -72,10 +72,14 @@ if __name__ == '__main__':
     usage_msg += "        Calculates the mean and SD for all observables\n"
     usage_msg += "        from a list of .gdat files and saves the results in\n"
     usage_msg += "        two pickle files, one for the mean and one for SD."
+    usage_msg += "    python ssa_job.py submit [run_script.py] [num_jobs]\n"
+    usage_msg += "        For use with LSF. Calls bsub to submit the desired\n"
+    usage_msg += "        number of instances of run_script.py."
 
     if len(sys.argv) < 2:
         print usage_msg
         sys.exit()
+    # Parse simulation output files
     if sys.argv[1] == 'parse':
         gdat_files = sys.argv[2:]
         if len(gdat_files) == 0:
@@ -88,6 +92,34 @@ if __name__ == '__main__':
             pickle.dump(means, f)
         with open('stds.pck', 'w') as f:
             pickle.dump(stds, f)
+    # Submit jobs to LSF
+    elif sys.argv[1] == 'submit':
+        if len(sys.argv) < 4:
+            print usage_msg
+            sys.exit()
+        if not sys.argv[1].endswith('.py'):
+            print "The run script must be a Python script with .py extension.\n"
+            print usage_msg
+            sys.exit()
+
+        run_script = sys.argv[2]
+        num_sims = int(sys.argv[3])
+        queue = 'mini'
+        time_limit = '00:10'
+        output_base = run_script.split('.')[0]
+
+        cmd_list = []
+        for i in range(num_sims):
+            output_filename = '%s_%d.out' % (output_base, i)
+
+            cmd_list = ['bsub',
+                        '-W', time_limit,
+                        '-q', queue,
+                        '-o', output_filename,
+                        'python', run_script]
+            print ' '.join(cmd_list)
+            subprocess.call(cmd_list)
+    # Unrecognized command
     else:
         print usage_msg
         sys.exit()
