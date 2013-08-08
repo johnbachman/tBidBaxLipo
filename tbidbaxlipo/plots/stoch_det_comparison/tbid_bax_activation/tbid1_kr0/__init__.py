6ba37aa8c0f199642454d83947691bf1dea953b8
John Bachman <bachmanjohn@gmail.com>
2013-08-07 20:53:48 -0400

    New simulations for tbid bax activation
    Also moved the run scripts to their own packages. Modified gitignore
    to ignore gdat, cdat, bngl, and net files produced by BNG.

---
+++ tbidbaxlipo/plots/stoch_det_comparison/tbid_bax_activation/tbid1_kr0/__init__.py
@@ -0,0 +1,35 @@
+from tbidbaxlipo.models import simulation
+import pickle
+import pkgutil
+
+class Job(simulation.Job):
+    def __init__(self):
+        params_dict = {'tBid_transloc_kr': 0, 'tBid_0':1}
+        scaling_factor = 20
+        tmax = 10000
+        num_sims = 30
+        n_steps = 100
+        super(Job, self).__init__(params_dict, scaling_factor, tmax, n_steps,
+                                  num_sims)
+
+    def build(self, module):
+        builder = module.Builder(params_dict=self.params_dict,
+                                 scaling_factor=self.scaling_factor)
+        builder.translocate_tBid_Bax()
+        builder.tBid_activates_Bax()
+        builder.model.name = job_name
+        return builder
+
+job = Job()
+job_name = 'tbid1_kr0'
+
+try:
+    n_cpt_obs = pickle.loads(pkgutil.get_data(
+        'tbidbaxlipo.plots.stoch_det_comparison.'
+        'tbid_bax_activation.%s' % job_name, 'n_cpt_obs.pck'))
+except IOError:
+    pass
+
+if __name__ == '__main__':
+    n_cpt_obs = job.run_n_cpt(cleanup=False)
+
