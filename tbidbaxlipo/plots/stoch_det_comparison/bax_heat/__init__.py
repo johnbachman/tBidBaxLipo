32ea0d1356e17f947c95c5f8c38dc1f35d182b1b
John Bachman <bachmanjohn@gmail.com>
2013-08-14 08:30:22 -0400

    Jobs, data for bax_heat (no auto activation)
    Unlike with auto activation, this agrees completely with the
    deterministic version.

---
+++ tbidbaxlipo/plots/stoch_det_comparison/bax_heat/__init__.py
@@ -0,0 +1,32 @@
+from tbidbaxlipo.models import simulation
+import pickle
+import os
+import sys
+
+mod_path = os.path.dirname(sys.modules[__name__].__file__)
+hdf5_filename = os.path.abspath(os.path.join(mod_path, 'data.hdf5'))
+if os.path.exists(hdf5_filename):
+    data = simulation.CptDataset(hdf5_filename)
+
+class Job(simulation.Job):
+    def __init__(self):
+        params_dict = {'tBid_transloc_kr': 0, 'tBid_0': 0}
+        scaling_factor = 5
+        tmax = 10000
+        num_sims = 10
+        n_steps = 100
+        super(Job, self).__init__(params_dict, scaling_factor, tmax, n_steps,
+                                  num_sims)
+
+    def build(self, module):
+        builder = module.Builder(params_dict=self.params_dict,
+                                 scaling_factor=self.scaling_factor)
+        builder.build_model_bax_heat()
+        return builder
+
+# Create jobs for each condition
+jobs = [Job()]
+job_name = 'bax_heat'
+
+if __name__ == '__main__':
+    simulation.run_main(jobs)
