82b84b7f31415ba9d044f2a2a1f2c93053320a76
John Bachman <bachmanjohn@gmail.com>
2013-08-27 23:15:49 -0400

    Analysis of 130614 Bax heat included in docs

--- doc/datasets/ants/130614_Bax_43C_titration.rst
+++ doc/datasets/ants/130614_Bax_43C_titration.rst
@@ -6,7 +6,53 @@ permeabilization in a unimolecular scenario (with no tBid) over a range of
 concentrations to create a dataset that could be used to constrain possible
 mechanisms.
 
+Raw data
+--------
+
 .. plot::
 
-    from tbidbaxlipo.plots.layout_130614 import main
-    main()
+    from tbidbaxlipo.plots.layout_130614 import plot_data
+    plot_data()
+
+Two-exponential fits
+--------------------
+
+Fits of each replicate to the three-parameter equation
+
+.. math::
+
+    F_{max} \left(1 - e^{-k_1 (1 - e^{-k_2 t})t}\right)
+
+.. plot::
+    :context:
+
+    from tbidbaxlipo.plots.layout_130614 import *
+    (fmax_arr, k1_arr, k2_arr, conc_list) = plot_two_exp_fits()
+
+:math:`F_{max}` curves
+----------------------
+
+.. plot::
+    :context:
+
+    plt.close('all')
+    plot_fmax_curve(fmax_arr, conc_list)
+
+:math:`k_1` curves
+------------------
+
+.. plot::
+    :context:
+
+    plt.close('all')
+    plot_k1_curve(k1_arr, conc_list)
+
+:math:`k_2` curves
+------------------
+
+.. plot::
+    :context:
+
+    plt.close('all')
+    plot_k2_curve(k2_arr, conc_list)
+
--- tbidbaxlipo/plots/layout_130614.py
+++ tbidbaxlipo/plots/layout_130614.py
@@ -5,6 +5,9 @@ import collections
 import sys
 import os
 import tbidbaxlipo.data
+from matplotlib import pyplot as plt
+import numpy as np
+from tbidbaxlipo.util import fitting
 
 def extract(keys, dict):
     extracted_dict = collections.OrderedDict([])
@@ -77,7 +80,7 @@ df = to_dataframe(reset_norm_means, reset_norm_sds)
 pores = get_average_pore_timecourses(reset_norm_means)
 """Average pores derived by taking -log(1 - data)."""
 
-def main():
+def plot_data():
     """Plots the data and various transformations of it."""
     ion()
 
@@ -110,3 +113,114 @@ def main():
     figure()
     plot_all(pores)
     title("Avg. pores per liposome")
+
+def plot_two_exp_fits():
+    data = norm_wells
+    fmax_arr = np.zeros((3, len(layout.keys())))
+    k1_arr = np.zeros((3, len(layout.keys())))
+    k2_arr = np.zeros((3, len(layout.keys())))
+    conc_list = np.zeros(len(layout.keys()))
+
+    for i, conc_str in enumerate(layout.keys()):
+        plt.figure()
+        wells = layout[conc_str]
+        well_conc = float(conc_str.split(' ')[1])
+        conc_list[i] = well_conc
+
+        for j, well in enumerate(wells):
+            well_data = data[well]
+            time = np.array(well_data[TIME])
+            y = np.array(well_data[VALUE])
+
+            fmax = fitting.Parameter(0.9)
+            k1 = fitting.Parameter(0.01)
+            k2 = fitting.Parameter(0.001)
+
+            def two_part_exp(t):
+                return (fmax() * (1 - np.exp(-k1() *
+                                  (1 - np.exp(-k2()*t)) * t)))
+
+            fitting.fit(two_part_exp, [fmax, k1, k2], y, time)
+
+            fmax_arr[j, i] = fmax()
+            k1_arr[j, i] = k1()
+            k2_arr[j, i] = k2()
+
+            plt.plot(time, y, 'b')
+            plt.plot(time, two_part_exp(time), 'r')
+            plt.title(well_conc)
+            plt.xticks([0, 2000, 4000, 6000, 8000])
+            plt.ylabel('% Release')
+            plt.xlabel('Time (sec)')
+
+    plt.show()
+    return (fmax_arr, k1_arr, k2_arr, conc_list)
+
+def plot_k1_curve(k1_arr, conc_list):
+    k1_means = np.mean(k1_arr, axis=0)
+    k1_sds = np.std(k1_arr, axis=0)
+
+    plt.figure()
+    plt.errorbar(conc_list, k1_means, yerr= k1_sds / np.sqrt(3), color='r',
+                 linestyle='', linewidth=2)
+    plt.title('$k_1$')
+    plt.xlabel('[Bax] (nM)')
+    plt.ylabel('$k_1\ (\mathrm{sec}^{-1})$')
+
+    vi = fitting.Parameter(0.05)
+    vf = fitting.Parameter(0.025)
+    tau = fitting.Parameter(0.02)
+    # Define fitting function
+    def biphasic(t):
+        return (vf()*t) + ( (vi() - vf()) *
+                            ((1 - np.exp(-tau()*t))/tau()) )
+    fitting.fit(biphasic, [vi, vf, tau], k1_means, conc_list)
+    plt.plot(conc_list, biphasic(conc_list), 'r', linewidth=2)
+    plt.text(35, 0.00054,
+            r'$v_f + (v_i - v_f) \left(\frac{1 - e^{-\tau t}}{\tau}\right)$')
+    plt.text(35, 0.00049, r'$v_i$ = %.3g sec$^{-1}$ nM$^{-1}$' % vi())
+    plt.text(35, 0.00044, r'$v_f$ = %.3g sec$^{-1}$ nM$^{-1}$' % vf())
+    plt.text(35, 0.00039, r'$\frac{1}{\tau}$ = %.2f nM' % (1/tau()))
+    plt.title('Biphasic fit of $k_1$')
+    plt.show()
+
+def plot_k2_curve(k2_arr, conc_list):
+    k2_means = np.mean(k2_arr, axis=0)
+    k2_sds = np.std(k2_arr, axis=0)
+    # Plot k2 on a linear scale
+    plt.figure()
+    plt.errorbar(conc_list, k2_means, yerr=k2_sds / np.sqrt(3))
+    plt.title('$k_2$')
+
+def plot_fmax_curve(fmax_arr, conc_list):
+    fmax_means = np.mean(fmax_arr, axis=0)
+    fmax_sds = np.std(fmax_arr, axis=0)
+
+    # Plot of Fmax
+    plt.figure()
+    plt.errorbar(conc_list, fmax_means, yerr=fmax_sds / np.sqrt(3),
+                 label='Data', linestyle='')
+    plt.ylabel(r'$F_{max}$ value (% Release)')
+    plt.xlabel('[Bax] (nM)')
+    plt.title(r'$F_{max}$')
+
+    # Try fitting the fmax values to a hill function
+    hill_vmax = fitting.Parameter(0.9)
+    kd = fitting.Parameter(100)
+    def hill(s):
+        return ((hill_vmax() * s) / (kd() + s))
+    fitting.fit(hill, [kd, hill_vmax], fmax_means, conc_list)
+    plt.plot(conc_list, hill(conc_list), 'r', linewidth=2, label='Hill fit')
+
+    plt.text(35, 0.93, '$K_D$ = %.2f' % kd(), fontsize=16)
+    plt.text(35, 0.99, '$V_{max}$ = %.2f' % hill_vmax(), fontsize=16)
+
+    # Try fitting the fmax values to an exponential function
+    exp_fmax = fitting.Parameter(1.0)
+    exp_k = fitting.Parameter(0.01)
+    def exp_func(s):
+        return exp_fmax() * (1 - np.exp(-exp_k()*s))
+    fitting.fit(exp_func, [exp_fmax, exp_k], fmax_means, conc_list)
+    plt.plot(conc_list, exp_func(conc_list), 'g', linewidth=2, label='Exp fit')
+    plt.legend(loc='lower right')
+    plt.show()
