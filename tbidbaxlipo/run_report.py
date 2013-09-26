c492a15575dd362161a7e4cf80fcfdb555333415
John Bachman <bachmanjohn@gmail.com>
2013-09-26 12:22:22 -0400

    Fix of run_report to use titration_fit reporter

--- tbidbaxlipo/run_report.py
+++ tbidbaxlipo/run_report.py
@@ -14,7 +14,7 @@ import glob
 from bayessb.report import Report
 import bayessb.report.reporters
 from tbidbaxlipo.reporters import knowledge, topology, experiment, residuals, \
-                                  bax_heat
+                                  titration_fits
 from tbidbaxlipo.mcmc import import_mcmc_groups
 import sys
 import pickle
@@ -30,7 +30,7 @@ print chain_file_list
 chain_dict = import_mcmc_groups(chain_file_list)
 
 # Run the reporters
-rep = Report(chain_dict, [bayessb.report.reporters, bax_heat])
+rep = Report(chain_dict, [bayessb.report.reporters, titration_fits])
 #rep = Report(chain_dict, [bayessb.report.reporters, residuals])
 #rep = Report(chain_dict, [topology, bayessb.report.reporters,
 #                          knowledge, experiment])
