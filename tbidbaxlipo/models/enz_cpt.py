c8c3fb40a58e0cfeefdde2c173170f94a9b13c08
John Bachman <bachmanjohn@gmail.com>
2014-04-23 13:11:53 -0400

    Model to try enzymatic dye release by iBax
    In an attempt to fit the Bid/Bim NBD terbium data using a mechanistic
    model.

---
+++ tbidbaxlipo/models/enz_cpt.py
@@ -0,0 +1,85 @@
+from tbidbaxlipo.models import core, one_cpt
+from bayessb.priors import Normal
+
+class Builder(one_cpt.Builder):
+
+    def declare_monomers(self):
+        """Declares signatures for tBid and Bax."""
+        self.monomer('tBid', ['bh3', 'loc'],
+                {'loc': ['c', 'm']})
+        self.monomer('Bax',
+                        ['bh3', 'a6', 'loc', 'lipo'],
+                        {'loc':  ['c', 'm', 'i', 'a', 'p', 'bh3expos']})
+        self.monomer('Vesicles', ['bax', 'dye'],
+                     {'dye': ['f', 'e']})
+
+    def __init__(self, params_dict=None, nbd_sites=None, scaling_factor=None):
+        # Sets self.model = Model(), and self.param_dict
+        core.Builder.__init__(self, params_dict=params_dict)
+
+        self.declare_monomers()
+
+        # COMPARTMENTS
+        solution = self.compartment('solution', dimension=3, parent=None)
+        self.compartment('ves', dimension=2, parent=solution)
+
+        # INITIAL CONDITIONS
+        self.parameter('Vesicles_0', 1, estimate=False)
+        #self.parameter('tBid_0', 20, estimate=False)
+        self.parameter('Bax_0', 100, estimate=False)
+
+        tBid = self['tBid']
+        Bax = self['Bax']
+        Vesicles = self['Vesicles']
+
+        #self.initial(tBid(loc='c', bh3=None) ** solution, self['tBid_0'])
+        self.initial(Bax(loc='c', bh3=None, a6=None, lipo=None) ** solution,
+                     self['Bax_0'])
+        self.initial(Vesicles(bax=None, dye='f') ** solution, self['Vesicles_0'])
+
+        # OBSERVABLES
+        #self.observable('ctBid', tBid(loc='c'))
+        #self.observable('mtBid', tBid(loc='m'))
+        self.observable('cBax', Bax(loc='c'))
+        self.observable('mBax', Bax(loc='m'))
+        self.observable('iBax', Bax(loc='i'))
+        self.observable('aBax', Bax(loc='a'))
+        # self.observable('tBidBax', tBid(bh3=1) % Bax(bh3=1))
+        # self.observable('Bax2', Bax(bh3=1) % Bax(bh3=1))
+        #self.observable('Baxbh3', Bax(bh3=1))
+        #self.observable('Bax4',
+        #     MatchOnce(Bax(loc='i', bh3=1, a6=3) % Bax(loc='i', bh3=1, a6=4) % 
+        #               Bax(loc='i', bh3=2, a6=3) % Bax(loc='i', bh3=2, a6=4)))
+        # Pore formation
+        #self.observable('pBax', Bax(loc='p'))
+        #self.observable('pores', Pores())
+
+        # SCALING PARAMETERS
+        #if nbd_sites is not None:
+        #    self.declare_nbd_scaling_parameters(nbd_sites)
+
+    def build_model_nbd_terbium(self):
+        # Bax gets to the membrane
+        self.translocate_Bax()
+        # Bax is activated
+        self.basal_Bax_activation(reversible=True)
+        # Get monomers from the model
+        Bax = self['Bax']
+        Vesicles = self['Vesicles']
+        # Bax catalytically permeabilizes vesicles
+        self.parameter('iBax_enz_release_k', 1e-2,
+                       prior=Normal(-2, 3))
+        self.rule('iBax_enz_release',
+                  Bax(loc='i') + Vesicles(dye='f') >>
+                  Bax(loc='i') + Vesicles(dye='e'),
+                  self['iBax_enz_release_k'])
+        # Bax converts to a pore-incompetent state
+        # Like c1->c2
+        self.parameter('iBax_to_pBax_kf', 1e-3,
+                       prior=Normal(-3, 3))
+        self.parameter('iBax_to_pBax_kr', 1e-4,
+                       prior=Normal(-4, 3))
+        self.rule('iBax_to_pBax',
+                  Bax(loc='i') <> Bax(loc='p'),
+                  self['iBax_to_pBax_kf'], self['iBax_to_pBax_kr'])
+
