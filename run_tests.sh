d5b31c2561ae13299f62ec714cacd97fc1c4d9bb
John Bachman <bachmanjohn@gmail.com>
2014-08-06 09:20:07 -0400

    Updated test running script at the top level

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,4 @@
-nosetests tbidbaxlipo.plots.nbd_analysis tbidbaxlipo.plots.grid_analysis
-nosetests --with-doctest tbidbaxlipo.util.plate_assay
+#nosetests tbidbaxlipo.plots.nbd_analysis tbidbaxlipo.plots.grid_analysis
+#nosetests --with-doctest tbidbaxlipo.util.plate_assay
+nosetests tbidbaxlipo.tests.test_one_cpt_models
+nosetests tbidbaxlipo.tests.test_multi_cpt_models
