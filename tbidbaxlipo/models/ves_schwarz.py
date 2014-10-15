07ed151f4759a20afe245290a01d9a88cdca8d93
John Bachman <bachmanjohn@gmail.com>
2014-10-15 13:05:52 -0400

    Renamed and cleaned up pore_agg
    This will be the first in a series of models that attempt to duplicate
    the simple schwarz models using a vesicle-based approach.

--- tbidbaxlipo/models/ves_schwarz.py
+++ tbidbaxlipo/models/ves_schwarz.py
@@ -1,5 +1,4 @@
 from pysb import *
-import math
 
 Model()
 
@@ -13,7 +12,6 @@ Parameter('Vesicles_0', 5.)
 Parameter('Bax_to_mem_kf', 1e-2)
 Parameter('Bax_to_mem_kr', 1e-1)
 Parameter('Bax_forms_pores_kf', 1e-3 / Vesicles_0.value)
-#Parameter('Bax_aggregates_at_pores_kf', 1e-3)
 
 Initial(Bax(conf='aq', pore='n'), Bax_0)
 Initial(Vesicles(pore='n'), Vesicles_0)
@@ -32,13 +30,6 @@ Rule('Bax_forms_pores',
      Bax(conf='mem', pore='n') + Vesicles(pore='y'),
      Bax_forms_pores_kf)
 
-"""
-Rule('Bax_aggregates_at_pores',
-     Bax(conf='aq', pore='n') + Vesicles(pore='y') >>
-     Vesicles(pore='y'),
-     Bax_aggregates_at_pores_kf)
-"""
-
 Observable('pVes', Vesicles(pore='y'))
 Observable('cBax', Bax(conf='aq', pore='n'))
 
@@ -50,20 +41,15 @@ if __name__ == '__main__':
     from pysb.integrate import Solver
     import numpy as np
     from matplotlib import pyplot as plt
+    from tbidbaxlipo.plots.stoch_det_comparison.bax_schwarz import jobs, data
+    from tbidbaxlipo.plots.stoch_det_comparison.plots import *
 
     plt.ion()
 
-    """
-    from tbidbaxlipo.plots.stoch_det_comparison.bax_schwarz_irreversible_agg_rate \
-            import jobs, data
-    """
-    from tbidbaxlipo.plots.stoch_det_comparison.bax_schwarz import jobs, data
-    from tbidbaxlipo.plots.stoch_det_comparison.plots import *
     plot_dye_release_titration(jobs, data)
 
     tmax = 4000
     t = np.linspace(0, tmax, 500)
     s1 = Solver(model, t)
     s1.run()
-
     plt.plot(t, s1.yexpr['DR'], linewidth=2, color='g', label='pore_agg')
