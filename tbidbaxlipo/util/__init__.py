9e31f5a94d6f1adcaa9bcf76d5e41ac218d9bd7d
John Bachman <bachmanjohn@gmail.com>
2014-10-17 15:09:50 -0400

    Added some useful settings for publication figs

--- tbidbaxlipo/util/__init__.py
+++ tbidbaxlipo/util/__init__.py
@@ -1,4 +1,21 @@
+import matplotlib
+
 colors = ['r', 'g', 'b', 'c', 'y', 'm', 'k']
 
 def color_iter():
     return iter(colors)
+# A colorblind safe, print friendly, and photocopy safe color scheme:
+# http://colorbrewer2.org/?type=diverging&scheme=PuOr&n=3
+fig_orange = '#f1a340'
+fig_gray = '#f7f7f7'
+fig_purple = '#998ec3'
+
+def set_fig_params_for_publication():
+    matplotlib.rcParams['font.size'] = 7
+    matplotlib.rcParams['font.sans-serif'] = 'Arial'
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['text.latex.preamble'] = [
+            r'\usepackage{helvet}',
+            r'\usepackage{sansmath}',
+            r'\sansmath']
+
