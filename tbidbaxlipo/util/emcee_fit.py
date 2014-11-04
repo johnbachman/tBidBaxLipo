7b9c9eddbd55ee8cc9625e5033066454c83ef069
John Bachman <bachmanjohn@gmail.com>
2014-11-04 15:21:55 -0500

    MAP output only for the lowest temp chain

--- tbidbaxlipo/util/emcee_fit.py
+++ tbidbaxlipo/util/emcee_fit.py
@@ -407,8 +407,7 @@ def pt_sample(gf, ntemps, nwalkers, burn_steps, sample_steps, thin=1,
         for p, lnprob, lnlike in sampler.sample(p0, iterations=sample_steps,
                                             thin=thin):
             if nstep % 10 == 0:
-                print("nstep %d of %d, MAP: %f" % (nstep, sample_steps,
-                                                   np.max(lnprob)))
+                print "nstep %d of %d, MAP: %f" % (nstep, sample_steps, lnprob[0])
             nstep +=1
     # Otherwise, do the burn-in first
     else:
@@ -417,8 +416,7 @@ def pt_sample(gf, ntemps, nwalkers, burn_steps, sample_steps, thin=1,
         for p, lnprob, lnlike in sampler.sample(p0, iterations=burn_steps,
                                 storechain=False):
             if nstep % 10 == 0:
-                print("nstep %d of %d, MAP: %f" % (nstep, sample_steps,
-                                                   np.max(lnprob)))
+                print "nstep %d of %d, MAP: %f" % (nstep, sample_steps, lnprob[0])
             nstep +=1
 
         sampler.reset()
@@ -428,9 +426,7 @@ def pt_sample(gf, ntemps, nwalkers, burn_steps, sample_steps, thin=1,
         for p, lnprob, lnlike in sampler.sample(p, lnprob0=lnprob, lnlike0=lnlike,
                                 iterations=sample_steps, thin=thin):
             if nstep % 10 == 0:
-                print("nstep %d of %d, MAP: %f" % (nstep, sample_steps,
-                                                   np.max(lnprob)))
-
+                print "nstep %d of %d, MAP: %f" % (nstep, sample_steps, lnprob[0])
             nstep +=1
 
     # Close the pool!
