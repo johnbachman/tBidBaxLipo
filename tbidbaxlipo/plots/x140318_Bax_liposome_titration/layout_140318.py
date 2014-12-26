a3473a1fd00a9ba5420bf17f445eb5e3cf0ac1d1
John Bachman <bachmanjohn@gmail.com>
2014-12-26 12:56:20 -0500

    Got basic plots back up and running
    ...with the data preprocessing moved to preprocess_data.py

--- tbidbaxlipo/plots/x140318_Bax_liposome_titration/layout_140318.py
+++ tbidbaxlipo/plots/x140318_Bax_liposome_titration/layout_140318.py
@@ -1,148 +1,41 @@
-from tbidbaxlipo.util.plate_assay import *
-import itertools
-import pickle
-import collections
 import sys
-import os
-import tbidbaxlipo.data
-from matplotlib import pyplot as plt
+import pickle
 import numpy as np
+from matplotlib import pyplot as plt
+from preprocess_data import timecourse_wells, lipo_bg_wells, bgsub_wells, \
+                            layout, lipo_bg_layout
+from tbidbaxlipo.util.plate_assay import plot_all, TIME, VALUE
+from tbidbaxlipo.plots.titration_fits import TwoExp, OneExpFmax
 from tbidbaxlipo.util import fitting, set_fig_params_for_publication, \
                              emcee_fit, format_axis
-from tbidbaxlipo.plots.titration_fits import TwoExp, OneExpFmax
-from matplotlib.ticker import MultipleLocator, ScalarFormatter
 from tbidbaxlipo.models.nbd import multiconf
 from tbidbaxlipo.models import one_cpt
 
-layout = collections.OrderedDict([
-        ('Bax 185 nM, Lipos 1 mg/ml',  ['A1']),
-        ('Bax 185 nM, Lipos 0.5 mg/ml',  ['A2']),
-        ('Bax 185 nM, Lipos 0.25 mg/ml',  ['A3']),
-        ('Bax 185 nM, Lipos 0.125 mg/ml',  ['A4']),
-        ('Bax 185 nM, Lipos 0.063 mg/ml',  ['A5']),
-        ('Bax 185 nM, Lipos 0.031 mg/ml',  ['A6']),
-        ('Bax 185 nM, Lipos 0.016 mg/ml',  ['A7']),
-        ('Bax 185 nM, Lipos 0.008 mg/ml',  ['A8']),
-        ('Bax 185 nM, Lipos 0.004 mg/ml',  ['A9']),
-        ('Bax 185 nM, Lipos 0.002 mg/ml',  ['A10']),
-        ('Bax 185 nM, Lipos 0.001 mg/ml',  ['A11']),
-        ('Bax 185 nM, Lipos 0 mg/ml',  ['A12']),
-        ('Lipos 1 mg/ml',  ['B1']),
-        ('Lipos 0.5 mg/ml',  ['B2']),
-        ('Lipos 0.25 mg/ml',  ['B3']),
-        ('Lipos 0.125 mg/ml',  ['B4']),
-        ('Lipos 0.063 mg/ml',  ['B5']),
-        ('Lipos 0.031 mg/ml',  ['B6']),
-        ('Lipos 0.016 mg/ml',  ['B7']),
-        ('Lipos 0.008 mg/ml',  ['B8']),
-        ('Lipos 0.004 mg/ml',  ['B9']),
-        ('Lipos 0.002 mg/ml',  ['B10']),
-        ('Lipos 0.001 mg/ml',  ['B11']),
-        ('Lipos 0 mg/ml',  ['B12']),
-    ])
-data_path = os.path.dirname(sys.modules['tbidbaxlipo.data'].__file__)
-timecourse_file = os.path.abspath(os.path.join(data_path,
-                                        '140318_NBD_Bax_BimBH3_lipo_titration.txt'))
-
-# Assemble all the wells included in the layout
-# http://stackoverflow.com/questions/406121/
-# flattening-a-shallow-list-in-python
-wells_to_read = list(itertools.chain(*layout.values()))
-
-# Timecourse wells
-timecourse_wells = read_flexstation_kinetics(timecourse_file)
-"""The raw (unnormalized) timecourses."""
-timecourse_wells = extract(wells_to_read, timecourse_wells)
-
-lipo_bg_conditions = [
-        'Lipos 1 mg/ml',
-        'Lipos 0.5 mg/ml',
-        'Lipos 0.25 mg/ml',
-        'Lipos 0.125 mg/ml',
-        'Lipos 0.063 mg/ml',
-        'Lipos 0.031 mg/ml',
-        'Lipos 0.016 mg/ml',
-        'Lipos 0.008 mg/ml',
-        'Lipos 0.004 mg/ml',
-        'Lipos 0.002 mg/ml',
-        'Lipos 0.001 mg/ml',
-        'Lipos 0 mg/ml']
-lipo_bg_layout = extract(lipo_bg_conditions, layout)
-lipo_bg_wells = extract([layout[cond][0] for cond in lipo_bg_conditions],
-                        timecourse_wells)
-
-bax_lipo_conditions = [
-        'Bax 185 nM, Lipos 1 mg/ml',
-        'Bax 185 nM, Lipos 0.5 mg/ml',
-        'Bax 185 nM, Lipos 0.25 mg/ml',
-        'Bax 185 nM, Lipos 0.125 mg/ml',
-        'Bax 185 nM, Lipos 0.063 mg/ml',
-        'Bax 185 nM, Lipos 0.031 mg/ml',
-        'Bax 185 nM, Lipos 0.016 mg/ml',
-        'Bax 185 nM, Lipos 0.008 mg/ml',
-        'Bax 185 nM, Lipos 0.004 mg/ml',
-        'Bax 185 nM, Lipos 0.002 mg/ml',
-        'Bax 185 nM, Lipos 0.001 mg/ml',
-        'Bax 185 nM, Lipos 0 mg/ml', ]
-bax_lipo_layout = extract(bax_lipo_conditions, layout)
-bax_lipo_wells = extract([layout[cond][0] for cond in bax_lipo_conditions],
-                         timecourse_wells)
-
-# Normalized and background subtracted
-bgsub_wells = subtract_background_set(bax_lipo_wells, lipo_bg_wells)
-
-(bgsub_averages, bgsub_sds) = averages(bgsub_wells, bax_lipo_layout)
-
-#bgsub_norm_wells = subtract_background(norm_wells, background)
-
-# Normalized, background subtracted, averaged
-#(bgsub_norm_averages, bgsub_norm_stds) = averages(bgsub_norm_wells, layout)
-
-# First timepoint shifted to 0 (better for fitting)
-#reset_bgsub_means = reset_first_timepoint_to_zero(bgsub_norm_averages)
-"""Timecourses normalized, BG-subtracted, averaged, then with first point
-shifted to t = 0."""
-#reset_bgsub_sds = reset_first_timepoint_to_zero(bgsub_norm_stds)
-
-lipo_conc_conv_factor = 15.502 # 1 mg/ml ~= 15.502 nM liposomes
-bg_tc = bgsub_averages['Bax 185 nM, Lipos 0 mg/ml'][VALUE]
-bg_time = bgsub_averages['Bax 185 nM, Lipos 0 mg/ml'][TIME]
-data_to_fit = []
-lipo_concs_to_fit = []
-lipo_mgs_to_fit = [1., 0.5, 0.25, 0.125, 0.063, 0.031, 0.016, 0.008, 0]
-for conc_name in bgsub_averages.keys():
-    lipo_mg = float(conc_name.split()[4])
-    if not lipo_mg in lipo_mgs_to_fit:
-        continue
-    lipo_concs_to_fit.append(lipo_mg * lipo_conc_conv_factor)
-    t = bgsub_averages[conc_name][TIME]
-    v = bgsub_averages[conc_name][VALUE]
-    v_bg = v / bg_tc
-    data_to_fit.append(v_bg)
-
 def plot_data():
     """Plots the data and various transformations of it."""
-    ion()
+    plt.ion()
 
     # Timecourse wells
-    figure()
+    plt.figure()
     plot_all(timecourse_wells)
-    title("Raw timecourses")
+    plt.title("Raw timecourses")
 
     # Lipo bg wells
-    figure()
+    plt.figure()
     plot_all(lipo_bg_wells)
-    title("Lipo background wells")
+    plt.title("Lipo background wells")
 
     # Background-subtracted wells
-    figure()
+    plt.figure()
     plot_all(bgsub_wells)
-    title("Background-subtracted wells")
+    plt.title("Background-subtracted wells")
 
 def plot_lipo_background(wells, layout):
     """Takes the lipo background well timecourses and plots the
     fluorescence values of the initial points as a function of lipo
-    concentration."""
+    concentration. Shows that the liposome background fluorescence
+    is strictly linear.
+    """
     init_vals = np.zeros(len(layout.keys()))
     conc_list = np.zeros(len(layout.keys()))
     for i, cond_name in enumerate(layout):
@@ -166,6 +59,8 @@ def plot_lipo_background(wells, layout):
     plt.xlabel('Liposome concentration (mg/ml)')
     plt.ylabel('RFU')
     plt.title('Liposome background fluorescence at t=0')
+    ax = plt.gca()
+    ax.set_xscale('log')
     print "m: %f" % m()
     print "b: %f" % b()
     return [m(), b()]
@@ -314,10 +209,7 @@ def mpi_fit():
         data.append(v_bg)
 
     (gf, sampler) = fit_with_2conf_mc(bg_time, data, lipo_concs)
-    #gf = fit_with_2conf(bg_time, data, lipo_concs)
-    #gf = fit_with_2conf_rev(bg_time, data, lipo_concs)
-    #gf = fit_with_3conf(bg_time, data, lipo_concs)
-    #gf = fit_with_simple_3conf(bg_time, data, lipo_concs)
+
     return (gf, sampler)
 
 def fit_with_simple_3conf(time, data, lipo_concs):
@@ -432,6 +324,8 @@ if __name__ == '__main__':
         triangle.corner(sampler.flatchain[0])
         plt.savefig('%s_tri.pdf' % output_base)
         sys.exit()
+
+    sys.exit()
 
     (gf2, sampler2) = pickle.load(open('../pt/pt_140318_nbd_2_conf_4.pck'))
     plot_emcee_fits(gf2, sampler2)
