96ccd9c568ebd30bf4dee4abde3d5fea3d2aae63
John Bachman <bachmanjohn@gmail.com>
2014-12-31 14:08:49 -0500

    Model pre-compilation script looks for all .yaml files
    And loads the models specified in those .yaml files.

--- tbidbaxlipo/plots/x140320_NBD_Bax_BimBH3_unlab_Bax_titration/compile_models.py
+++ tbidbaxlipo/plots/x140320_NBD_Bax_BimBH3_unlab_Bax_titration/compile_models.py
@@ -1,12 +1,21 @@
+"""Load all of the .yaml files in this directory, create models from them,
+and give a pilot run to pre-compile the RHS functions."""
+
 from pysb.integrate import Solver
 import numpy as np
+import yaml
+import glob
 
 from tbidbaxlipo.models.one_cpt import Builder
 
-bd = Builder()
+yaml_filenames = glob.glob('./*.yaml')
 
-bd.build_model_nbd_2_conf_dimer()
+for yaml_filename in yaml_filenames:
+    with open(yaml_filename) as yaml_file:
+        args = yaml.load(yaml_file)
 
-t = np.linspace(0, 1e4, 1e3)
-sol = Solver(bd.model, t)
-sol.run()
+    bd = Builder()
+    bd.build_model_from_dict(args['model'])
+    t = np.linspace(0, 1e4, 1e3)
+    sol = Solver(bd.model, t)
+    sol.run()
