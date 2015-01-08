518225bd280cd01a733181173d218a86db77ab74
John Bachman <bachmanjohn@gmail.com>
2015-01-08 14:13:33 -0500

    Refactored plot_data function
    Now uses plt. prefixes and also the imports have been cleaned up

--- tbidbaxlipo/plots/x141119_Bax_Bid_saturation/layout_141119.py
+++ tbidbaxlipo/plots/x141119_Bax_Bid_saturation/layout_141119.py
@@ -1,4 +1,10 @@
-from tbidbaxlipo.util.plate_assay import *
+from tbidbaxlipo.util.plate_assay import plot_all
+from matplotlib import pyplot as plt
+from preprocess_data import bim_bh3, bid_80, bid_40, bid_20, bid_10, \
+                            bid_5, bid_2, bid_0
+import sys
+
+"""
 import itertools
 import pickle
 import collections
@@ -16,152 +22,41 @@ from scipy.stats import linregress
 from tbidbaxlipo.plots import titration_fits as tf
 from tbidbaxlipo.util import fitting, colors, set_fig_params_for_publication, \
                              format_axis
-
-# All wells with liposomes ~0.15 mg/mL
-
-data_path = os.path.dirname(sys.modules['tbidbaxlipo.data'].__file__)
-timecourse_file = os.path.abspath(os.path.join(data_path,
-                                  '141119_Bax_Bid_saturation_timecourse.txt'))
-
-bax_concs = np.array([1000., 500., 250., 125., 62.5, 31.2, 15.6, 7.8,
-                      3.9, 2.0, 0.]) + 25.
-
-# Timecourse wells
-timecourse_wells = read_flexstation_kinetics(timecourse_file)
-"""The raw (unnormalized) timecourses."""
-
-def time_offset_vector():
-    read_time = 320 # seconds (5:20)
-    row_times = {'A': 0,
-                 'B': 20,
-                 'C': 37,
-                 'D': 60,
-                 'E': 80,
-                 'F': 100,
-                 'G': 130,
-                 'H': 150,
-             }
-    offset_dict = {}
-    # New time vector is TIME + offset, where
-    # offset = (read_time - row_time)
-    for row in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
-        for col in range(1, 13):
-            well = '%s%s' % (row, col)
-            offset_dict[well] = read_time - row_times[row]
-    return offset_dict
-
-def add_offset_vector(timecourse_wells, offset_vector):
-    for well_name in timecourse_wells.keys():
-        well = timecourse_wells[well_name]
-        well[TIME] += offset_vector[well_name]
-
-def row_wells(row_name, max_col=12):
-    return ['%s%s' % (row_name, i) for i in range(1, max_col+1)]
-
-# Should update time vector of timecourse wells in place
-add_offset_vector(timecourse_wells, time_offset_vector())
-
-no_nbd_lipo_well_names = ['B12', 'D12', 'F12'] # Ignoring H12 as an outlier
-no_nbd_lipo_wells = extract(no_nbd_lipo_well_names, timecourse_wells)
-
-no_lipo_well_names = ['A12', 'C12', 'E12', 'G12']
-no_lipo_wells = extract(no_lipo_well_names, timecourse_wells)
-
-bg_layout = collections.OrderedDict([
-         ('No lipos', ['A12', 'C12', 'E12', 'G12']),
-         ('No NBD or lipos', ['B12', 'D12', 'F12']),
-         ('NBD and lipos', ['G9', 'G10', 'G11']),
-        ])
-(bg_averages, bg_std) = averages(timecourse_wells, bg_layout)
-
-# Difference between NBD-Bax and background (buffer only) gives fluorescence
-# of NBD-Bax alone
-bg_diff = {}
-bg_diff['BG diff'] = []
-bg_diff['BG diff'].append(bg_averages['No lipos'][TIME])
-bg_diff['BG diff'].append(bg_averages['No lipos'][VALUE] -
-                          bg_averages['No NBD or lipos'][VALUE])
-bg_diff['BG diff2'] = []
-bg_diff['BG diff2'].append(bg_averages['NBD and lipos'][TIME])
-bg_diff['BG diff2'].append(bg_averages['NBD and lipos'][VALUE] -
-                           bg_averages['No lipos'][VALUE])
-bg_diff['BG diff3'] = []
-bg_diff['BG diff3'].append(bg_averages['No NBD or lipos'][TIME])
-bg_diff['BG diff3'].append(bg_averages['No NBD or lipos'][VALUE] + 2.5)
-
-# So liposomes alone has fluorescence of roughly 2.55
-# So need to subtract background and 2.55 from each curve to get
-# baseline fluorescence. The other curves are normalized against this.
-
-bg_to_subtract = bg_averages['No NBD or lipos'][VALUE] + 2.5
-
-bg_sub_wells = subtract_background(timecourse_wells, bg_to_subtract)
-
-bid_80 = extract(row_wells('A', 11), bg_sub_wells)
-bid_40 = extract(row_wells('B', 11), bg_sub_wells)
-bid_20 = extract(row_wells('C', 11), bg_sub_wells)
-bid_10 = extract(row_wells('D', 11), bg_sub_wells)
-bid_5 = extract(row_wells('E', 11), bg_sub_wells)
-bid_2 = extract(row_wells('F', 11), bg_sub_wells)
-bid_0 = extract(row_wells('G', 11), bg_sub_wells)
-bim_bh3 = extract(row_wells('H', 11), bg_sub_wells)
+"""
 
 def plot_data():
     """Plots the data and various transformations of it."""
-    """
-    figure()
-    plot_all(no_nbd_lipo_wells)
-    title("No NBD-Bax or Liposomes")
-
-    # Lipo bg wells
-    figure()
-    plot_all(no_lipo_wells)
-    title("No liposomes")
-
-    # Lipo bg wells
-    figure()
-    plot_all(bg_averages, errors=bg_std)
-    title("Background")
-
-    # BG difference
-    figure()
-    plot_all(bg_diff)
-
-    figure()
-    plot_all(bg_sub_wells)
-    """
-
-    figure()
+    plt.figure()
     plot_all(bim_bh3)
-    title('Bim BH3')
+    plt.title('Bim BH3')
 
-    figure()
+    plt.figure()
     plot_all(bid_80)
-    title('Bid 80nM')
+    plt.title('Bid 80nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_40)
-    title('Bid 40 nM')
+    plt.title('Bid 40 nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_20)
-    title('Bid 20 nM')
+    plt.title('Bid 20 nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_10)
-    title('Bid 10 nM')
+    plt.title('Bid 10 nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_5)
-    title('Bid 5 nM')
+    plt.title('Bid 5 nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_2)
-    title('Bid 2 nM')
+    plt.title('Bid 2 nM')
 
-    figure()
+    plt.figure()
     plot_all(bid_0)
-    title('Bid 0 nM')
+    plt.title('Bid 0 nM')
 
 def plot_mm(k_data, bax_concs, bid_concs):
     plt.figure('mm fit', figsize=(1.5, 1.5), dpi=300)
@@ -223,7 +118,8 @@ def plot_mm(k_data, bax_concs, bid_concs):
 
 if __name__ == '__main__':
     plt.ion()
-    #plot_data()
+    plot_data()
+    sys.exit()
 
     set_fig_params_for_publication()
 
