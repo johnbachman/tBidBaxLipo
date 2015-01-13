4fc849ec2292c081aecb8aa5d637ece0af092d12
John Bachman <bachmanjohn@gmail.com>
2015-01-13 12:27:58 -0500

    Format data matrices for fitting

--- tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
+++ tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
@@ -9,5 +9,14 @@ outliers = [21, 24, 77, 103, 108, 113]
 for outlier_ix in outliers:
     df[('54', 'FRET', 'VALUE')][outlier_ix] = np.nan
 
+# Matrices to use in fitting
+time_54 = df[('54', 'FRET', 'TIME')].values
+data_54 = np.zeros((1, 2, len(time_54)))
+data_54[0, 0, :] = df[('54', 'NBD', 'VALUE')].values
+data_54[0, 1, :] = df[('54', 'FRET', 'VALUE')].values
 
+time_126 = df[('126', 'FRET', 'TIME')].values
+data_126 = np.zeros((1, 2, len(time_126)))
+data_126[0, 0, :] = df[('126', 'NBD', 'VALUE')].values
+data_126[0, 1, :] = df[('126', 'FRET', 'VALUE')].values
 
