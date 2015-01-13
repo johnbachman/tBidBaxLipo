337addc24ec391c82814b31193abbc9e03614339
John Bachman <bachmanjohn@gmail.com>
2015-01-13 14:23:52 -0500

    Filter NaN padding out of 54C data

--- tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
+++ tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
@@ -10,10 +10,14 @@ for outlier_ix in outliers:
     df[('54', 'FRET', 'VALUE')][outlier_ix] = np.nan
 
 # Matrices to use in fitting
-time_54 = df[('54', 'FRET', 'TIME')].values
+
+# Filter NaN padding out from time and value matrices
+time_54_nans = df[('54', 'FRET', 'TIME')].values
+time_54 = time_54_nans[~np.isnan(time_54_nans)]
+
 data_54 = np.zeros((1, 2, len(time_54)))
-data_54[0, 0, :] = df[('54', 'NBD', 'VALUE')].values
-data_54[0, 1, :] = df[('54', 'FRET', 'VALUE')].values
+data_54[0, 0, :] = df[('54', 'NBD', 'VALUE')].values[~np.isnan(time_54_nans)]
+data_54[0, 1, :] = df[('54', 'FRET', 'VALUE')].values[~np.isnan(time_54_nans)]
 
 time_126 = df[('126', 'FRET', 'TIME')].values
 data_126 = np.zeros((1, 2, len(time_126)))
