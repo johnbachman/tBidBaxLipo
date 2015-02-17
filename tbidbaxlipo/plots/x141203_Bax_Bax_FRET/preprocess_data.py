ea6f70ab8343d0438be8ce9c50f84ff3b9c1f3be
John Bachman <bachmanjohn@gmail.com>
2015-02-17 15:37:41 -0500

    Updated outlier removal script, added plot feature
    Remove a few outliers from 54C FRET and one outlier from 54C NBD.
    Currently no outliers for 126C.

--- tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
+++ tbidbaxlipo/plots/x141203_Bax_Bax_FRET/preprocess_data.py
@@ -1,26 +1,62 @@
 from parse_data import df
 import numpy as np
+from copy import deepcopy
+from matplotlib import pyplot as plt
 
 # There are a number of outliers in the FRET data that Justin attributes
 # to debris floating in the cuvette that produces momentary spikes. Here
 # we explicitly remove several of these from the 54C data, setting them to
 # NaN.
-outliers = [21, 24, 77, 103, 108, 113]
-for outlier_ix in outliers:
-    df[('54', 'FRET', 'VALUE')][outlier_ix] = np.nan
+df_pre = deepcopy(df)
+
+# Outliers in 54C FRET data
+outliers_54_FRET = [21, 24, 77, 103, 108, 113]
+for outlier_ix in outliers_54_FRET:
+    df_pre[('54', 'FRET', 'VALUE')][outlier_ix] = np.nan
+
+# Outliers in 54C NBD data
+outliers_54_NBD = [109]
+for outlier_ix in outliers_54_NBD:
+    df_pre[('54', 'NBD', 'VALUE')][outlier_ix] = np.nan
+
+# NOTE: There are currently no really major outliers for 126C.
 
 # Matrices to use in fitting
-
-# Filter NaN padding out from time and value matrices
-time_54_nans = df[('54', 'FRET', 'TIME')].values
-time_54 = time_54_nans[~np.isnan(time_54_nans)]
-
+time_54 = df_pre[('54', 'FRET', 'TIME')].values
 data_54 = np.zeros((1, 2, len(time_54)))
-data_54[0, 0, :] = df[('54', 'NBD', 'VALUE')].values[~np.isnan(time_54_nans)]
-data_54[0, 1, :] = df[('54', 'FRET', 'VALUE')].values[~np.isnan(time_54_nans)]
+data_54[0, 0, :] = df_pre[('54', 'NBD', 'VALUE')].values
+data_54[0, 1, :] = df_pre[('54', 'FRET', 'VALUE')].values
 
-time_126 = df[('126', 'FRET', 'TIME')].values
+time_126 = df_pre[('126', 'FRET', 'TIME')].values
 data_126 = np.zeros((1, 2, len(time_126)))
-data_126[0, 0, :] = df[('126', 'NBD', 'VALUE')].values
-data_126[0, 1, :] = df[('126', 'FRET', 'VALUE')].values
+data_126[0, 0, :] = df_pre[('126', 'NBD', 'VALUE')].values
+data_126[0, 1, :] = df_pre[('126', 'FRET', 'VALUE')].values
 
+def plot_outliers():
+    plt.ion()
+    # 54C FRET
+    plt.figure()
+    plt.plot(df[('54', 'FRET', 'VALUE')].values, 'r')
+    plt.plot(df_pre[('54', 'FRET', 'VALUE')].values, 'b')
+    plt.title('54C FRET')
+
+    # 54C NBD
+    plt.figure()
+    plt.plot(df[('54', 'NBD', 'VALUE')].values, 'r')
+    plt.plot(df_pre[('54', 'NBD', 'VALUE')].values, 'b')
+    plt.title('54C NBD')
+
+    # 126C FRET
+    plt.figure()
+    plt.plot(df[('126', 'FRET', 'VALUE')].values, 'r')
+    plt.plot(df_pre[('126', 'FRET', 'VALUE')].values, 'b')
+    plt.title('126C FRET')
+
+    # 126C NBD
+    plt.figure()
+    plt.plot(df[('126', 'NBD', 'VALUE')].values, 'r')
+    plt.plot(df_pre[('126', 'NBD', 'VALUE')].values, 'b')
+    plt.title('126C NBD')
+
+if __name__ == '__main__':
+    plot_outliers()
