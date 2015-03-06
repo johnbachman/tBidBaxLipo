8b11ed8662d372f56c986afa01d09a5d4d88344c
John Bachman <bachmanjohn@gmail.com>
2015-03-06 13:43:44 -0500

    Func to calc error variance
    Uses the LinearIntercept, Quadratic, and Cubic fit objects in
    plots/titration_fits, and then makes a three-panel plot showing the fit,
    the residuals histogram, and Q-Q plot against a normal distribution.

--- tbidbaxlipo/util/calculate_error_variance.py
+++ tbidbaxlipo/util/calculate_error_variance.py
@@ -1,101 +1,46 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from tbidbaxlipo.util import fitting
+import scipy.stats
+import tbidbaxlipo.plots.titration_fits as tf
 
-def calc_err_var_linear(data, last_n_pts=50, plot=False):
-    a1 = fitting.Parameter(0.005)
-    b = fitting.Parameter(1.)
-    def fit_func(t):
-        return a1()*t + b()
+def calc_err_var(data, last_n_pts=50, fit_type='cubic', plot=False):
+    # Prune the data
     t = np.arange(len(data))
     data_subset = data[-last_n_pts:]
     t_subset = t[-last_n_pts:]
-    result = fitting.fit(fit_func, [a1, b], data_subset, t_subset,
-                         log_transform=False)
-    residuals = result[0]
+    # Get the appropriate fit object
+    if fit_type == 'linear':
+        fit = tf.LinearIntercept(log_transform=False)
+    elif fit_type == 'quadratic':
+        fit = tf.Quadratic(log_transform=False)
+    elif fit_type == 'cubic':
+        fit = tf.Cubic(log_transform=False)
+    else:
+        raise ValueError("Unknown fit type! Must be 'linear', 'quadratic', or "
+                         "'cubic'.")
+    # Run the fit
+    params = fit.fit_timecourse(t_subset, data_subset)
+    print params
+    # Get the best-fit curve
+    ypred = fit.fit_func(t_subset, params)
+    # Calculate residuals
+    residuals = data_subset - ypred
+    # Plot results
+    fig = None
     if plot:
-        plt.figure()
-        plt.subplot(1, 2, 1)
+        # Plot of fit and distribution of residuals
+        fig = plt.figure(figsize=(12, 5))
+        plt.subplot(1, 3, 1)
         plt.plot(t, data)
-        plt.plot(t_subset, fit_func(t_subset))
-        plt.subplot(1, 2, 2)
+        plt.plot(t_subset, ypred)
+        plt.title('Fit of %s to last %s pts' % (fit_type, last_n_pts))
+        plt.subplot(1, 3, 2)
         plt.hist(residuals)
-    return np.std(residuals)
-
-def calc_err_var_quadratic(data, last_n_pts=50, plot=False):
-    a1 = fitting.Parameter(0.001)
-    a2 = fitting.Parameter(0.005)
-    b = fitting.Parameter(1.)
-    def fit_func(t):
-        return a1()*t**2 + a2()*t + b()
-
-    t = np.arange(len(data))
-    data_subset = data[-last_n_pts:]
-    t_subset = t[-last_n_pts:]
-    result = fitting.fit(fit_func, [a1, a2, b], data_subset, t_subset,
-                         log_transform=False)
-    residuals = result[0]
-    if plot:
-        plt.figure()
-        plt.subplot(1, 2, 1)
-        plt.plot(t, data)
-        plt.plot(t_subset, fit_func(t_subset))
-        plt.subplot(1, 2, 2)
-        plt.hist(residuals)
-    return np.std(residuals)
-
-def calc_err_var_cubic(data, last_n_pts=50, plot=False):
-    a1 = fitting.Parameter(0.0001)
-    a2 = fitting.Parameter(0.001)
-    a3 = fitting.Parameter(0.005)
-    b = fitting.Parameter(1.)
-    def fit_func(t):
-        return a1()*t**3 + a2()*t**2 + a3()*t + b()
-
-    t = np.arange(len(data))
-    data_subset = data[-last_n_pts:]
-    t_subset = t[-last_n_pts:]
-    result = fitting.fit(fit_func, [a1, a2, a3, b], data_subset, t_subset,
-                         log_transform=False)
-    residuals = result[0]
-    if plot:
-        plt.figure()
-        plt.subplot(1, 2, 1)
-        plt.plot(t, data)
-        plt.plot(t_subset, fit_func(t_subset))
-        plt.subplot(1, 2, 2)
-        plt.hist(residuals)
-    return np.std(residuals)
-
-if __name__ == '__main__':
-
-    from tbidbaxlipo.plots.x141203_Bax_Bax_FRET.preprocess_data \
-                                                import data_126, data_54
-
-    plt.ion()
-
-    nbd_126_data = data_126[0, 0, :]
-    nbd_126_std = calc_err_var_cubic(nbd_126_data, last_n_pts=80, plot=True)
-    lbl = "126C NBD, sigma: %s" % nbd_126_std
-    plt.title(lbl)
-    print lbl
-
-    nbd_54_data = data_54[0, 0, :]
-    nbd_54_std = calc_err_var_cubic(nbd_54_data, last_n_pts=80, plot=True)
-    lbl = "54C NBD, sigma: %s" % nbd_54_std
-    plt.title(lbl)
-    print lbl
-
-    fret_126_data = data_126[0, 1, :]
-    fret_126_std = calc_err_var_cubic(fret_126_data, last_n_pts=80, plot=True)
-    lbl = "126C FRET, sigma: %s" % fret_126_std
-    plt.title(lbl)
-    print lbl
-
-    fret_54_data = data_54[0, 1, :]
-    fret_54_std = calc_err_var_cubic(fret_54_data, last_n_pts=80, plot=True)
-    lbl = "54C FRET, sigma: %s" % fret_54_std
-    plt.title(lbl)
-    print lbl
-
+        plt.title('Histogram of residuals')
+        plt.subplot(1, 3, 3)
+        scipy.stats.probplot(residuals, dist='norm', plot=plt)
+        plt.title('Quantile-quantile plot vs. normal')
+        plt.tight_layout()
+    return (np.std(residuals), fig)
 
