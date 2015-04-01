099c28d7a618f1d6a4152e0ff70905f1f7c841d1
John Bachman <bachmanjohn@gmail.com>
2015-04-01 09:49:07 -0400

    Add temp value to  data_sigma_var in preprocess_data

--- tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
+++ tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
@@ -24,19 +24,28 @@ for nbd_residue in nbd_residues:
         continue
     # Enumerate over the replicates
     for rep_ix, rep_num in enumerate(range(1, 4)):
+        time_var = df[(activator, obs, nbd_residue, rep_num, 'TIME')].values
         # The fitting procedure expects a three-dimensional array for each
         # entry; the first dimension is reserved for multiple concentrations,
         # as in a titration, which we don't have; the second is reserved for
         # multiple observables, which we also don't have for multiconf
         # fitting, and the third contains the timepoints.
-        time_var = df[(activator, obs, nbd_residue, rep_num, 'TIME')].values
         data_var = np.zeros((1, 1, len(time_var)))
         data_var[0, 0, :] = \
                 df[(activator, obs, nbd_residue, rep_num, 'VALUE')].values
+        # For the error estimates, the fitting procedure expects a 2-D array,
+        # with potentially distinct error estimates for each condition
+        # (first axis) and observable (second axis)
+        data_sigma_var = np.array([[0.021085]])
+        # Variable names
         time_var_name = 'time_%s_%s_%s_r%s' % \
                         (activator, obs, nbd_residue, rep_num)
         data_var_name = 'data_%s_%s_%s_r%s' % \
                         (activator, obs, nbd_residue, rep_num)
+        data_sigma_var_name = 'data_sigma_%s_%s_%s_r%s' % \
+                        (activator, obs, nbd_residue, rep_num)
+        # Add these as module-level variables
         setattr(this_module, time_var_name, time_var)
         setattr(this_module, data_var_name, data_var)
+        setattr(this_module, data_sigma_var_name, data_sigma_var)
 
