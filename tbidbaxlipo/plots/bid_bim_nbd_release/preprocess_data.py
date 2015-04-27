384a8a6436661b4a7f821b8f2a6d1c35ecf0a31f
John Bachman <bachmanjohn@gmail.com>
2015-04-27 16:15:25 -0400

    Calculate exp err across all reps during preprocessing

--- tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
+++ tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from tbidbaxlipo.data.parse_bid_bim_nbd_release import df, nbd_residues
+from tbidbaxlipo.util.calculate_error_variance import calc_err_var
 from copy import deepcopy
 
 this_module = sys.modules[__name__]
@@ -10,6 +11,7 @@ this_module = sys.modules[__name__]
 
 # Remove outliers
 pass
+
 
 # Instead of creating a list of ~60 variables for every replicate of every
 # residue, we add entries to the __dict__ variable for this namespace,
@@ -22,6 +24,10 @@ for nbd_residue in nbd_residues:
     # Skip the WT residue, since there's no NBD timecourse for it
     if nbd_residue == 'WT':
         continue
+    # To estimate the experimental error, we fit each timecourse to a polynomial
+    # equation and return the residuals; after processing all replicates, we
+    # pool the residuals and calculate the standard error
+    residuals_reps = []
     # Enumerate over the replicates
     for rep_ix, rep_num in enumerate(range(1, 4)):
         time_var = df[(activator, obs, nbd_residue, rep_num, 'TIME')].values
@@ -31,21 +37,31 @@ for nbd_residue in nbd_residues:
         # multiple observables, which we also don't have for multiconf
         # fitting, and the third contains the timepoints.
         data_var = np.zeros((1, 1, len(time_var)))
-        data_var[0, 0, :] = \
-                df[(activator, obs, nbd_residue, rep_num, 'VALUE')].values
-        # For the error estimates, the fitting procedure expects a 2-D array,
-        # with potentially distinct error estimates for each condition
-        # (first axis) and observable (second axis)
-        data_sigma_var = np.array([[0.021085]])
+        timecourse = df[(activator, obs, nbd_residue, rep_num, 'VALUE')].values
+        data_var[0, 0, :] = timecourse
+        # Get the residuals for this rep
+        (residuals, fig) = \
+           calc_err_var(timecourse, last_n_pts=50, fit_type='cubic', plot=False)
+        # Add the residuals for this rep to the list
+        residuals_reps.append(residuals)
         # Variable names
         time_var_name = 'time_%s_%s_%s_r%s' % \
                         (activator, obs, nbd_residue, rep_num)
         data_var_name = 'data_%s_%s_%s_r%s' % \
                         (activator, obs, nbd_residue, rep_num)
-        data_sigma_var_name = 'data_sigma_%s_%s_%s_r%s' % \
-                        (activator, obs, nbd_residue, rep_num)
         # Add these as module-level variables
         setattr(this_module, time_var_name, time_var)
         setattr(this_module, data_var_name, data_var)
-        setattr(this_module, data_sigma_var_name, data_sigma_var)
+
+    # Now that we've got the residuals for all reps, pool them and calculate
+    # the standard deviation
+    pooled_residuals = np.array(residuals_reps).flatten()
+    pooled_err = np.std(pooled_residuals, ddof=1)
+    # For the error estimates, the fitting procedure expects a 2-D array,
+    # with potentially distinct error estimates for each condition
+    # (first axis) and observable (second axis)
+    data_sigma_var = np.array([[pooled_err]])
+    # Add a module-level variable for the experimental error
+    data_sigma_var_name = 'data_sigma_%s_%s_%s' % (activator, obs, nbd_residue)
+    setattr(this_module, data_sigma_var_name, data_sigma_var)
 
