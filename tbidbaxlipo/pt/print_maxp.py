2dd4c05157a5c3c06f7a262bea43d6ed2378f8fc
John Bachman <bachmanjohn@gmail.com>
2015-04-29 16:37:12 -0400

    Print evidence for a set of .mcmc files

--- tbidbaxlipo/pt/print_maxp.py
+++ tbidbaxlipo/pt/print_maxp.py
@@ -8,6 +8,7 @@ import numpy as np
 
 file_list = sys.argv[1:]
 maxp_list = []
+evidence_list = []
 for filename in file_list:
     print("Loading: %s" % filename)
     basename = os.path.basename(filename)
@@ -18,10 +19,16 @@ for filename in file_list:
     maxp_ix = np.unravel_index(np.argmax(lnprob), lnprob.shape)
     maxp = lnprob[maxp_ix[0], maxp_ix[1]]
     maxp_list.append((basename, maxp))
+    evidence = sampler.thermodynamic_integration_log_evidence()
+    evidence_list.append((basename, evidence))
 
 # Sort on the probability
 sorted_maxp = sorted(maxp_list, key=lambda tup: tup[1], reverse=True)
-
-print("")
+sorted_evidence = sorted(evidence_list, key=lambda tup: tup[1], reverse=True)
+print("--- Max a posteriori: ---")
 for (filename, maxp) in sorted_maxp:
     print("%s: %s" % (maxp, filename))
+print("\n")
+print("--- log(evidence) ---")
+for (filename, evidence) in sorted_evidence:
+    print("%s: %s" % (evidence, filename))
