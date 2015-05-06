5a77adde6822bed252cd06dd74d53193ba8c6046
John Bachman <bachmanjohn@gmail.com>
2015-05-06 15:33:35 -0400

    Fix quotation in ssh command

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -9,7 +9,7 @@ num_nodes=$2
 starcluster put $cluster provision_node.sh /home/sgeadmin
 starcluster sshmaster $cluster '/home/sgeadmin/provision_node.sh'
 for (( i=1; i<num_nodes; i++ )); do
-    starcluster sshnode $cluster $(printf 'node%03d' $i) \
-        '/home/sgeadmin/provision_node.sh' &
+    starcluster sshmaster $cluster "ssh $(printf 'node%03d' $i) \
+        bash -l /home/sgeadmin/provision_node.sh" &
 done
 
