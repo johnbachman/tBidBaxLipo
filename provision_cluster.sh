42aa7d6a65d150e83965364d05b93396c833707a
John Bachman <bachmanjohn@gmail.com>
2015-05-06 13:40:47 -0400

    Scripts to initialize master and nodes

--- provision_cluster.sh
+++ provision_cluster.sh
@@ -1,26 +1,12 @@
-# Exit immediately if a command exits with error
-set +e
+#!/bin/sh
 
-# Put any temp/downloaded files into /home/sgeadmin
-cd /home/sgeadmin
+cluster=$1
+num_nodes=$2
 
-# Recompile mpi4py against openmpi
-pip uninstall -y mpi4py
-update-alternatives --set mpi /usr/lib/openmpi/include
-pip install mpi4py
-
-# Install packages for PySB
-pip install sympy
-pip install git+https://github.com/pysb/pysb.git
-curl -O -J http://mmbios.org/index.php/bionetgen-2-2-5-stable/bionetgen-2-2-5-stable-zip?format=raw
-unzip bionetgen-2.2.5-stable.zip
-mv BioNetGen-2.2.5-stable/ /usr/local/share/BioNetGen
-
-# Test PySB
-python -m pysb.examples.run_tutorial_a
-
-# Install additional software
-pip install emcee
-git clone https://github.com/johnbachman/tBidBaxLipo.git
-
+starcluster put $cluster provision_node.sh /home/sgeadmin
+starcluster sshmaster cluster '/home/sgeadmin/provision_node.sh'
+for (( i=1; i<num_nodes; i++ )); do
+    starcluster sshnode mycluster $(printf 'node%03d' $i) \
+        '/home/sgeadmin/provision_node.sh' &
+done
 
---
+++ provision_node.sh
@@ -0,0 +1,34 @@
+# Exit immediately if a command exits with error
+set +e
+
+if [[ $(hostname) == master ]]; then
+    # Put any temp/downloaded files into /home/sgeadmin
+    cd /home/sgeadmin
+    curl -O -J http://mmbios.org/index.php/bionetgen-2-2-5-stable/bionetgen-2-2-5-stable-zip?format=raw
+    unzip bionetgen-2.2.5-stable.zip
+    # Get project code
+    git clone https://github.com/johnbachman/tBidBaxLipo.git
+    # FIXME
+    git checkout starcluster
+    #pip install -e tBidBaxLipo
+
+cd /tmp
+
+# Recompile mpi4py against openmpi
+pip uninstall -y mpi4py
+update-alternatives --set mpi /usr/lib/openmpi/include
+pip install mpi4py
+
+# Install additional software
+pip install emcee
+
+# Install packages for PySB
+pip install sympy
+pip install git+https://github.com/pysb/pysb.git
+# Put BNG into the right location for this node
+cp -a /home/sgeadmin/BioNetGen-2.2.5-stable/ /usr/local/share/BioNetGen
+
+# Test PySB
+python -m pysb.examples.run_tutorial_a
+
+
