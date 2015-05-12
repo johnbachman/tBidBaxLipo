41476971b2562f8d37391d7bf55d64c9a924e60c
John Bachman <bachmanjohn@gmail.com>
2015-05-12 12:22:58 -0400

    Plotting func for conformations

--- tbidbaxlipo/pt/show_chain.py
+++ tbidbaxlipo/pt/show_chain.py
@@ -107,6 +107,73 @@ def plot_emcee_fits(gf, sampler, sample=True, burn=None, nsamples=100):
 
         format_axis(ax)
 
+def plot_conformations(gf, sampler, sample=True, burn=None, nsamples=100):
+    """Plot fluorescence conformations from the MCMC chain vs. the data."""
+    set_fig_params_for_publication()
+
+    # If we're plotting samples, get the indices now and use them for
+    # all observables
+    if sample:
+        (nwalkers, nsteps) = sampler.chain.shape[1:3]
+        if burn is None:
+            burn = int(nsteps / 2)
+
+        walker_indices = np.random.randint(0, nwalkers, size=nsamples)
+        step_indices = np.random.randint(burn, nsteps, size=nsamples)
+
+    for obs_ix in range(gf.data.shape[1]):
+        fig = plt.figure(figsize=(3, 3), dpi=300)
+        plt.ylabel('$F/F_0$')
+        plt.xlabel(r'Time (sec $\times 10^3$)')
+        #plt.ylim([0.7, 5.2])
+        plt.xlim([0, gf.time[-1] + 500])
+        ax = plt.gca()
+        #ax.set_xticks(np.linspace(0, 1e4, 6))
+        #ax.set_xticklabels([int(f) for f in np.linspace(0, 10, 6)])
+        plt.subplots_adjust(bottom=0.24, left=0.21)
+        # Plot the different observables
+        for cond_ix in range(gf.data.shape[0]):
+            data = gf.data[cond_ix, obs_ix, :]
+            plt.plot(gf.time, data, 'k', linewidth=1)
+        # Colors for different observables
+        obs_colors = ['r', 'g', 'b', 'm', 'k']
+
+        def plot_confs(x, plot_args):
+            for cond_ix in range(gf.data.shape[0]):
+                gf.set_parameters(x, obs_ix=obs_ix, cond_ix=cond_ix)
+                gf.solver.run()
+                # Iterate over the number of conformations
+                for conf_ix in range(gf.builder.num_confs):
+                    conf_y = gf.solver.yobs['Bax_c%d' % conf_ix]
+                    conf_scaling = \
+                       gf.builder.model.parameters['c%d_scaling' %
+                                                     conf_ix].value
+                    y = conf_y * conf_scaling
+                    if 'color' not in plot_args:
+                        plt.plot(gf.solver.tspan, y, label='c%d' % conf_ix,
+                                 color=obs_colors[conf_ix], **plot_args)
+                    else:
+                        plt.plot(gf.solver.tspan, y, label='c%d' % conf_ix,
+                                 **plot_args)
+
+        # If we're plotting samples:
+        if sample:
+            for i in xrange(nsamples):
+                p = sampler.chain[0, walker_indices[i], step_indices[i], :]
+                plot_args = {'alpha': 0.1}
+                plot_confs(p, plot_args=plot_args)
+
+        # Plot the maximum a posteriori fit
+        maxp_flat_ix = np.argmax(sampler.lnprobability[0])
+        maxp_ix = np.unravel_index(maxp_flat_ix,
+                                   sampler.lnprobability[0].shape)
+        maxp = sampler.lnprobability[0][maxp_ix]
+        plot_args = {'color': 'k', 'alpha': 1, 'linewidth':0.5}
+        plot_confs(sampler.chain[0, maxp_ix[0], maxp_ix[1]],
+                   plot_args=plot_args)
+
+        format_axis(ax)
+
 if __name__ == '__main__':
 
     usage_msg =  "Usage:\n"
@@ -124,8 +191,9 @@ if __name__ == '__main__':
 
     # Show plots
     plt.ion()
-    triangle_plots(gf, sampler)
-    plot_chain_convergence(sampler)
+    #triangle_plots(gf, sampler)
+    #plot_chain_convergence(sampler)
     plot_emcee_fits(gf, sampler, burn=None, sample=True)
+    plot_conformations(gf, sampler, burn=None, sample=True)
     #plot_emcee_fits_subplots(gf, sampler)
 
