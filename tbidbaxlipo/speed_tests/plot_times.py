9c5f7861420029de209afe043436a7a04384041a
John Bachman <bachmanjohn@gmail.com>
2015-05-22 16:14:30 -0400

    Made plot_times more general

--- tbidbaxlipo/speed_tests/plot_times.py
+++ tbidbaxlipo/speed_tests/plot_times.py
@@ -9,10 +9,11 @@ if len(sys.argv) < 3:
 plot_filename = sys.argv[1]
 num_reps = int(sys.argv[2])
 
-num_entries = 15
+num_procs_list = range(16, 65, 4)
+#num_procs_list = range(2, 2 + num_entries)
+num_entries = len(num_procs_list)
 results = np.zeros((num_entries, num_reps))
-num_procs_list = range(2, 2 + num_entries)
-nworkers_list = range(1, 1 + num_entries)
+nworkers_list = [procs - 1 for procs in num_procs_list]
 for ix, num_procs in enumerate(num_procs_list):
     filename = '%s.txt' % num_procs
     with open(filename) as f:
@@ -26,10 +27,10 @@ plt.ion()
 
 if num_reps > 1:
     sd = np.std(results, axis=1, ddof=1)
-    plt.errorbar(nworkers_list, m, yerr=sd)
+    plt.errorbar(nworkers_list, m, yerr=sd, marker='o')
 else:
-    plt.plot(nworkers_list, m)
-
+    plt.plot(nworkers_list, m, marker='o')
+    plt.ylim([0, None])
 plt.xlabel("Num workers")
-plt.ylabel("Normalized 1-worker time, 100 MCMC steps (sec)")
+plt.ylabel("Normalized 1-worker time, MCMC steps (sec)")
 plt.savefig('%s.pdf' % plot_filename)
