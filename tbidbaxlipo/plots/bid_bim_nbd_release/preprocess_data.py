28c532dbb6fe7a1b9c909dc4a34c16eab903fd15
John Bachman <bachmanjohn@gmail.com>
2015-06-03 14:10:52 -0400

    Wrap preprocess_data to 80 chars

--- tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
+++ tbidbaxlipo/plots/bid_bim_nbd_release/preprocess_data.py
@@ -26,27 +26,32 @@ for activator in activators:
         # Skip the WT residue, since there's no NBD timecourse for it
         if nbd_residue == 'WT':
             continue
-        # To estimate the experimental error, we fit each timecourse to a polynomial
-        # equation and return the residuals; after processing all replicates, we
-        # pool the residuals and calculate the standard error
+        # To estimate the experimental error, we fit each timecourse to a
+        # polynomial equation and return the residuals; after processing all
+        # replicates, we pool the residuals and calculate the standard error
         residuals_reps = []
         # Enumerate over the replicates
         for rep_ix, rep_num in enumerate(range(1, 4)):
-            time_var = df[(activator, obs, nbd_residue, rep_num, 'TIME')].values
+            time_var = \
+                     df[(activator, obs, nbd_residue, rep_num, 'TIME')].values
             # The fitting procedure expects a three-dimensional array for each
-            # entry; the first dimension is reserved for multiple concentrations,
-            # as in a titration, which we don't have; the second is reserved for
-            # multiple observables, which we also don't have for multiconf
-            # fitting, and the third contains the timepoints.
+            # entry; the first dimension is reserved for multiple
+            # concentrations, as in a titration, which we don't have; the
+            # second is reserved for multiple observables, which we also
+            # don't have for multiconf fitting, and the third contains the
+            # timepoints.
             data_var = np.zeros((1, 1, len(time_var)))
-            timecourse = df[(activator, obs, nbd_residue, rep_num, 'VALUE')].values
+            timecourse = \
+                    df[(activator, obs, nbd_residue, rep_num, 'VALUE')].values
             data_var[0, 0, :] = timecourse
             # Initial fluorescence value
             f0 = timecourse[0]
-            # Get the minimum fluorescence for this rep: 0.1 * the initial value
+            # Get the minimum fluorescence for this rep: 0.1 * the initial
+            # value
             lbound = f0 * 0.1
-            # Get the maximum fluorescence for this rep: the lesser of 10 times
-            # the initial value or a corrected absolute NBD fluorescence of 300k
+            # Get the maximum fluorescence for this rep: the lesser of 10
+            # times the initial value or a corrected absolute NBD
+            # fluorescence of 300k
             max_nbd_fluorescence = 300000
             mutant_absolute_max = \
                     labeling_ratios[nbd_residue] * max_nbd_fluorescence
@@ -54,7 +59,8 @@ for activator in activators:
             ubound = min(mutant_absolute_max, mutant_relative_max)
             # Get the residuals for this rep
             (residuals, fig) = \
-               calc_err_var(timecourse, last_n_pts=50, fit_type='cubic', plot=False)
+               calc_err_var(timecourse, last_n_pts=50, fit_type='cubic',
+                            plot=False)
             # Add the residuals for this rep to the list
             residuals_reps.append(residuals)
             # Variable names
@@ -73,8 +79,8 @@ for activator in activators:
             setattr(this_module, ubound_name, ubound)
             setattr(this_module, f0_name, f0)
 
-        # Now that we've got the residuals for all reps, pool them and calculate
-        # the standard deviation
+        # Now that we've got the residuals for all reps, pool them and
+        # calculate the standard deviation
         pooled_residuals = np.array(residuals_reps).flatten()
         pooled_err = np.std(pooled_residuals, ddof=1)
         # For the error estimates, the fitting procedure expects a 2-D array,
@@ -82,6 +88,7 @@ for activator in activators:
         # (first axis) and observable (second axis)
         data_sigma_var = np.array([[pooled_err]])
         # Add a module-level variable for the experimental error
-        data_sigma_var_name = 'data_sigma_%s_%s_%s' % (activator, obs, nbd_residue)
+        data_sigma_var_name = 'data_sigma_%s_%s_%s' % (activator, obs,
+                                                       nbd_residue)
         setattr(this_module, data_sigma_var_name, data_sigma_var)
 
