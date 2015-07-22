94015dec71924f0b7fce05b73e5ab6b96ab0e18d
John Bachman <bachmanjohn@gmail.com>
2015-07-22 17:11:05 -0400

    Fixed test_model_from_dict.py so tests pass

--- tbidbaxlipo/models/test_model_from_dict.py
+++ tbidbaxlipo/models/test_model_from_dict.py
@@ -10,14 +10,14 @@ def test_key_error_on_unknown_feature():
 
 def test_model_name_sort_order():
     model_dict = {'activation': 1,
-                  'translocation': 1}
+                  'baxtranslocation': 1}
     bd = Builder()
     bd.build_model_from_dict(model_dict)
-    eq_(bd.model_name, 'Tra1Act1')
+    eq_(bd.model.name, 'Baxtr1Activ1')
 
 @raises(ValueError)
 def test_error_on_unknown_implementation():
-    model_dict = {'translocation': 9}
+    model_dict = {'baxtranslocation': 9}
     bd = Builder()
     bd.build_model_from_dict(model_dict)
 
@@ -25,15 +25,17 @@ def test_error_on_unknown_implementation():
 for the presence of named rules. Brittle if rule names later change."""
 def test_translocation_1():
     """Check translocation by checking for Bax_mono_translocates_sol_to_ves."""
-    model_dict = {'translocation': 1}
+    model_dict = {'baxtranslocation': 1,
+                  'bidtranslocation': 1}
     bd = Builder()
     bd.build_model_from_dict(model_dict)
+    print bd.model.rules
     ok_(bd.model.rules['tBid_translocates_sol_to_ves'])
     ok_(bd.model.rules['Bax_mono_translocates_sol_to_ves'])
 
 @raises(KeyError)
 def test_error_on_bleach_without_nbd():
-    model_dict = {'translocation': 1,
+    model_dict = {'baxtranslocation': 1,
                   'activation': 1,
                   'bleach': 1}
     bd = Builder()
