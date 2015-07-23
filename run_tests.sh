8675e958a1166156149dad1f4f34b5e7186169f9
John Bachman <bachmanjohn@gmail.com>
2015-07-23 14:47:29 -0400

    Switched test script to run_tests.sh
    Because this doesn't scan and import the whole code tree, it means I can
    comment out (for the time being) the modules that have dependencies such
    as h5py, which takes a long time to build.

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #nosetests tbidbaxlipo.plots.nbd_analysis tbidbaxlipo.plots.grid_analysis
+#nosetests tbidbaxlipo.tests.test_one_cpt_models
+#nosetests tbidbaxlipo.tests.test_multi_cpt_models
 #nosetests --with-doctest tbidbaxlipo.util.plate_assay
-nosetests tbidbaxlipo.tests.test_one_cpt_models
-nosetests tbidbaxlipo.tests.test_multi_cpt_models
+nosetests tbidbaxlipo.plots.x140318_Bax_liposome_titration.test_140318
