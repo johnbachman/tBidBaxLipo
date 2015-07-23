424d69f84b69d962c07f9bd99233c924c5b3b4ea
John Bachman <bachmanjohn@gmail.com>
2015-07-22 23:02:51 -0400

    Moved arg parsing outside of run function
    So that run() can be run outside of a command-line setting.

--- tbidbaxlipo/pt/run_pt.py
+++ tbidbaxlipo/pt/run_pt.py
@@ -5,7 +5,7 @@ import os
 from tbidbaxlipo.util import emcee_fit
 import numpy as np
 
-def run(args, mpi=True):
+def run(args, mcmc_filename, random_seed, pos_filename, mpi=True):
     """Takes dict of args and initializes PT run.
 
     The args can come from a YAML file or be passed in by a script.
@@ -14,7 +14,6 @@ def run(args, mpi=True):
     gf = emcee_fit.global_fit_from_args(args)
 
     # Seed the random number generator
-    random_seed = int(sys.argv[2])
     np.random.seed(random_seed)
 
     # Make the beta ladder
@@ -22,7 +21,6 @@ def run(args, mpi=True):
     betas = 10 ** np.linspace(0, args['highest_temp'], ntemps)
 
     ### RUN the sampler!
-    pos_filename = sys.argv[3]
     # If the file does exist, load the position as the start position
     if os.path.isfile(pos_filename):
         # Open the position/random state file
@@ -51,16 +49,8 @@ def run(args, mpi=True):
     # After sampling, get rid of the pool so we can pickle the sampler
     sampler.pool = None
 
-    # The basename of the pickle file is based on the name of the .yaml file
-    basedir = os.path.dirname(sys.argv[1])
-    basename = os.path.basename(sys.argv[1])
-    basename = basename.split('.')[0]
-    filename = os.path.join(basedir, '%s.mcmc' % basename)
-    with open(filename, 'w') as f:
+    with open(mcmc_filename, 'w') as f:
         pickle.dump((gf, sampler), f)
-
-    # Done
-    sys.exit()
 
 if __name__ == '__main__':
     # Parameters for the job are specified in a YAML file, which should be
@@ -74,4 +64,13 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as yaml_file:
         args = yaml.load(yaml_file)
 
-    run(args, mpi=True)
+    # The basename of the pickle file is based on the name of the .yaml file
+    basedir = os.path.dirname(sys.argv[1])
+    basename = os.path.basename(sys.argv[1])
+    basename = basename.split('.')[0]
+    mcmc_filename = os.path.join(basedir, '%s.mcmc' % basename)
+    # Get the random seed and the filename to pickle the current position to
+    random_seed = int(sys.argv[2])
+    pos_filename = sys.argv[3]
+
+    run(args, mcmc_filename, random_seed, pos_filename, mpi=True)
