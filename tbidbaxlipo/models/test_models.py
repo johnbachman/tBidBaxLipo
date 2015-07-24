61134864d2bafd4364158b423e19d07058ac3ad3
John Bachman <bachmanjohn@gmail.com>
2015-07-24 11:36:45 -0400

    Test for lipo_sites_builder

---
+++ tbidbaxlipo/models/test_models.py
@@ -0,0 +1,18 @@
+from nose.tools import ok_
+from tbidbaxlipo.models import one_cpt, lipo_sites
+
+
+def test_lipo_sites_builder():
+    """lipo_sites_builder builds and runs without error."""
+    lsb = lipo_sites.Builder()
+
+def test_one_cpt_lipo_sites_comparison():
+    """Simulation of one_cpt and the lipo_sites model with Bax_0 numbers
+    of sites per liposome should give the same result."""
+    # A simulation of
+    ocb = one_cpt.Builder()
+    lsb = lipo_sites.Builder()
+
+if __name__ == '__main__':
+    test_lipo_sites_builder()
+    #test_one_cpt_lipo_sites_comparison()
