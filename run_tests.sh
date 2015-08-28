8d5a3854ee338f82af71d2cee5d0cc7a5c1cf91d
John Bachman <bachmanjohn@gmail.com>
2015-08-28 16:57:28 -0400

    Test kale sphinx docs build

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,10 @@
 #nosetests tbidbaxlipo.tests.test_multi_cpt_models
 #nosetests --with-doctest tbidbaxlipo.util.plate_assay
 export BASEDIR=`pwd`
-cd $BASEDIR/kale/make
-make
-#cd $BASEDIR/kale/doc
-#make html
+#cd $BASEDIR/kale/make
+#make
+cd $BASEDIR/kale/doc
+make html
 #cd $BASEDIR
 #nosetests tbidbaxlipo/plots/x140318_Bax_liposome_titration
 #nosetests tbidbaxlipo/plots/x140320_NBD_Bax_BimBH3_unlab_Bax_titration
