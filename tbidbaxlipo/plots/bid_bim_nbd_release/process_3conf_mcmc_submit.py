674ff68729d48bcf3424de3c76c8494d7fee5afc
John Bachman <bachmanjohn@gmail.com>
2015-09-25 20:58:40 +0000

    Update 3conf submit to reflect changes

--- tbidbaxlipo/plots/bid_bim_nbd_release/process_3conf_mcmc_submit.py
+++ tbidbaxlipo/plots/bid_bim_nbd_release/process_3conf_mcmc_submit.py
@@ -3,10 +3,10 @@ import subprocess
 
 filelist = sys.argv[1:]
 for filename in filelist:
-    outfile = '%s.c1_peak.out' % filename.split('.')[0]
-    errfile = '%s.c1_peak.err' % filename.split('.')[0]
-    cmd = 'qsub -b y -cwd -V -o %s -e %s ' \
-          'python -m tbidbaxlipo.plots.bid_bim_nbd_release.c1_peak_times %s' % \
+    outfile = '%s.process_3conf.out' % filename.split('.')[0]
+    errfile = '%s.process_3conf.err' % filename.split('.')[0]
+    cmd = 'qsub -b y -cwd -V -o %s -e %s python -m ' \
+          'tbidbaxlipo.plots.bid_bim_nbd_release.process_3conf_mcmc %s' % \
           (outfile, errfile, filename)
     cmd_list = cmd.split(' ')
     p = subprocess.Popen(cmd_list)
