2576aeea163bb1b8658d32efd1723a6f2dd74f20
John Bachman <bachmanjohn@gmail.com>
2015-11-01 18:11:26 +0000

    Closed form soln for NBD/FRET, 4confs

--- tbidbaxlipo/models/nbd/multiconf.py
+++ tbidbaxlipo/models/nbd/multiconf.py
@@ -293,6 +293,36 @@ class Builder(core.Builder):
                            self['fret1_scaling'].value * c1 +
                            self['fret2_scaling'].value * (Bax_0 - c0 - c1))
                     return {'NBD': nbd, 'FRET': fret}
+        elif self.num_confs == 4 and self.reversible == False:
+            # Here we don't specifically handle the case where the parameters are
+            # equal since it's so unlikely to come up in parameter estimation,
+            # which is the immediate concern
+            def nbd_fret_func(t):
+                k1 = self['c0_to_c1_k'].value
+                k2 = self['c1_to_c2_k'].value
+                k3 = self['c2_to_c3_k'].value
+                if k1 == k2 or k2 == k3 or k1 == k3:
+                    raise ValueError('Function for equal values of k1, k2, or '
+                                     'k3 is not currently implemented.')
+                Bax_0 = self['Bax_0'].value
+                c0 = np.exp(-k1*t)*Bax_0
+                c1 = (((np.exp(-k1*t) - np.exp(-k2*t)) * k1 * Bax_0) /
+                      (k2 - k1))
+                c2 = ((np.exp(-(k1 + k2 + k3) * t) * k1 * k2 *
+                       (np.exp((k1+k2)*t) * (k1 - k2) +
+                        np.exp((k2+k3)*t) * (k2 - k3) +
+                        np.exp((k1+k3)*t) * (k3 - k1)) * Bax_0) /
+                        ((k1 - k2) * (k1 - k3) * (k2 - k3)))
+                c3 = Bax_0 - c0 - c1 - c2
+                nbd = (self['c0_scaling'].value * c0 +
+                       self['c1_scaling'].value * c1 +
+                       self['c2_scaling'].value * c2 +
+                       self['c3_scaling'].value * c3)
+                fret = (self['fret0_scaling'].value * c0 +
+                       self['fret1_scaling'].value * c1 +
+                       self['fret2_scaling'].value * c2 +
+                       self['fret3_scaling'].value * c3)
+                return {'NBD': nbd, 'FRET': fret}
         # If we don't fit one of these categories, set to None
         else:
             nbd_fret_func = None
