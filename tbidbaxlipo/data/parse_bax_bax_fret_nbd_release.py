0ff20a4eeabcd854a3923176eef3d937ffccf90d
John Bachman <bachmanjohn@gmail.com>
2017-09-20 11:09:27 -0400

    Parse script for new dataset

--- tbidbaxlipo/data/parse_bax_bax_fret_nbd_release.py
+++ tbidbaxlipo/data/parse_bax_bax_fret_nbd_release.py
@@ -9,21 +9,29 @@ import os
 import sys
 
 data_path = os.path.dirname(sys.modules['tbidbaxlipo.data'].__file__)
-data_file = os.path.abspath(os.path.join(data_path,
-        '2016 - Bax-Bax FRET dataset.xlsx'))
+
+if True:
+    data_file = os.path.abspath(os.path.join(data_path,
+            '2017-09-19 - BAX-BAX FRET dataset V2 Compiled.xlsx'))
+    # Zero-indexed
+    FIRST_ROW_INDEX = 8
+    LAST_ROW_INDEX = 134
+else:
+    data_file = os.path.abspath(os.path.join(data_path,
+            '2016 - Bax-Bax FRET dataset.xlsx'))
+    # Zero-indexed
+    FIRST_ROW_INDEX = 3
+    LAST_ROW_INDEX = 128
 
 # These lists must match the order in the spreadsheet exactly
 nbd_residues = ['3', '36', '54', '68', '126', '134', '120',
                 '175', '179']
 activators = ['Bid']
-datatypes = ['Release', 'FRET', 'NBD']
+#datatypes = ['Release', 'FRET', 'NBD']
+datatypes_time = ['Time', 'Release', 'FRET', 'NBD']
 reps = [1, 2, 3]
 col_types = ['TIME', 'VALUE']
 
-# Zero-indexed
-FIRST_ROW_INDEX = 3
-# Should be 128, but chopped off the last few points to avoid massive outliers
-LAST_ROW_INDEX = 128
 
 wb = load_workbook(data_file, data_only=True)
 
@@ -41,24 +49,29 @@ activator = 'Bid'
 for nbd_ix, nbd_residue in enumerate(nbd_residues):
     # ...and replicates...
     for rep_ix, rep in enumerate(reps):
-        # Now iterate over the Release, FRET, NBD columns
-        for dtype_ix, dtype in enumerate(datatypes):
-            for col_type_ix, col_type in enumerate(col_types):
-                # Iterate and fill the array of column values
-                vector = []
-                data_col = sheet.columns[col_index]\
-                                         [FIRST_ROW_INDEX:LAST_ROW_INDEX]
-                factor = 100 if ((dtype == 'Release' or dtype == 'FRET') \
-                                 and col_type == 'VALUE') else 1
-                for cell in data_col:
-                    if (cell.value == None):
-                        vector.append(np.nan)
-                    else:
-                        vector.append(float(cell.value) * factor)
-                col_tuples.append((activator, dtype, nbd_residue, rep,
-                                   col_type))
+        time_vector = None
+        # Now iterate over the Time, Release, FRET, NBD columns
+        for dtype_ix, dtype in enumerate(datatypes_time):
+            # Iterate and fill the array of column values
+            vector = []
+            data_col = sheet.columns[col_index]\
+                                     [FIRST_ROW_INDEX:LAST_ROW_INDEX]
+            for cell in data_col:
+                if (cell.value == None):
+                    vector.append(np.nan)
+                else:
+                    vector.append(float(cell.value))
+            if dtype_ix == 0:
+                time_vector = vector
+            else:
+                assert time_vector is not None
+                time_tuple = (activator, dtype, nbd_residue, rep, 'TIME')
+                value_tuple = (activator, dtype, nbd_residue, rep, 'VALUE')
+                col_tuples.append(time_tuple)
+                col_tuples.append(value_tuple)
+                data.append(time_vector)
                 data.append(vector)
-                col_index += 1
+            col_index += 1
             # Increment the index of the current column
 
 # Create the Pandas dataframe
